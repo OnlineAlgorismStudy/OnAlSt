0270677684f365fc77fca77f81070c1ca839527a
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-06-29 17:44:18 +0900

    Add files via upload

---
+++ src/question/day0629/R_06_29.java
@@ -0,0 +1,23 @@
+
+public class R_06_29 {
+
+	public static void main(String[] args) {
+	  
+		int kor = 90;
+		int mat = 80;
+		int eng = 100;
+		
+		int sum = kor + mat + eng;
+		int avg = sum / 3;
+		
+		System.out.println("kor = " + kor);
+		System.out.println("mat = " + mat);
+		System.out.println("eng = " + eng);
+		
+		System.out.println("sum = " + sum + "\n" + "avg = " + avg);
+		
+		
+		
+	}
+
+}
