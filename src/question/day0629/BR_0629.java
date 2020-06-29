42106c073581a2ea13d5cdf9adb7fd54c44debe9
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-06-29 16:57:13 +0900

    BR0629_java

---
+++ src/question/day0629/BR_0629.java
@@ -0,0 +1,20 @@
+
+public class BR_0629 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kor = 90;
+		int mat = 80;
+		int eng = 100;
+		
+		System.out.println("kor " + kor);
+		System.out.println("mat " + mat);
+		System.out.println("eng " + eng);
+		
+		int sum = kor + mat + eng;
+		
+		System.out.println("sum " + sum);
+		System.out.println("avg " + (int)(sum/3));
+	}
+
+}
