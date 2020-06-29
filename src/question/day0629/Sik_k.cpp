b4ed8a78817ef1ffabce04bce6686989028fbcf8
hanju39 <67551046+hanju39@users.noreply.github.com>
2020-06-29 15:57:24 +0900

    Create Sik_k.cpp

---
+++ src/question/day0629/Sik_k.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int kor = 90;
+	int mat = 80;
+	int eng = 100;
+	int sum = kor + mat + eng;
+	int avg = sum / 3;
+	printf ("kor %d\n", kor);
+	printf ("mat %d\n", mat);
+	printf ("eng %d\n", eng);
+	printf ("sum %d\n", sum); 
+	printf ("avg %d\n", avg);
+	
+	return 0;
+}
