d6c0e49b168f991b0541e93744f634927a7ec6c8
Ren <67572250+i-am-ren@users.noreply.github.com>
2020-06-29 20:24:26 +0900

    Update ren_0629_solution.kt

--- src/question/day0629/ren_0629_solution.kt
+++ src/question/day0629/ren_0629_solution.kt
@@ -4,7 +4,7 @@ fun main() {
     val eng = 100;
     println("kor $kor")
     println("mat $mat")
-    println("eng eng")
+    println("eng $eng")
     println("sum ${kor + mat + eng}")
     println("avg ${(kor + mat + eng)/ 3}")
 }
