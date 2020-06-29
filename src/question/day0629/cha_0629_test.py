b41ddb6564478670842e0ae5b1f90fba55217288
JiyeonCha <66763635+JiyeonCha@users.noreply.github.com>
2020-06-29 22:08:18 +0900

    Add files via upload

---
+++ src/question/day0629/cha_0629_test.py
@@ -0,0 +1,8 @@
+strList =['kor', 'mat', 'eng']
+numList =[90, 80, 100]
+print (strList[0], numList[0])
+print (strList[1], numList[1])
+print (strList[2], numList[2])
+print ('sum', sum (numList))
+average = sum (numList) / len (numList) 
+print ('avg', average)
