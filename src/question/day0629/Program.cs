1f4b6165f86730cfc2a7ddb8839e23e17e85b9fa
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-06-29 17:26:12 +0900

    ImPaper_0629.cs

---
+++ src/question/day0629/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImPaper_0629
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int kor = 90;
+            int mat = 80;
+            int eng = 100;
+            int sum = kor + mat + eng;
+            Console.WriteLine("kor " + kor);
+            Console.WriteLine("mat " + mat);
+            Console.WriteLine("eng " + eng);
+            Console.WriteLine("sum " + (kor + mat + eng));
+            Console.WriteLine("avg " + (sum / 3));
+
+        }
+    }
+}
