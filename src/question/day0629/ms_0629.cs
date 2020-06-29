5aaa50582ebaa1622f0dda86bf105de433a9eadf
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-06-29 16:29:55 +0900

    algorithm 1 c#

---
+++ src/question/day0629/ms_0629.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace algorithm_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kor = 90;
+            var mat = 80;
+            var eng = 100;
+            var sum = kor + mat + eng;
+            Console.WriteLine($"kor {kor}\nmat {mat}\neng {eng}\nsum {sum}\navg {sum/3}");
+        }
+    }
+}
