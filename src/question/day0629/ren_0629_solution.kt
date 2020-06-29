c52f19ceb728f8554f51d446ec0bfb3d897f457e
Ren <67572250+i-am-ren@users.noreply.github.com>
2020-06-29 20:23:29 +0900

    Add 0629 solution file
    6월 29일 알고리즘 풀이 파일 추가

---
+++ src/question/day0629/ren_0629_solution.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val kor = 90;
+    val mat = 80;
+    val eng = 100;
+    println("kor $kor")
+    println("mat $mat")
+    println("eng eng")
+    println("sum ${kor + mat + eng}")
+    println("avg ${(kor + mat + eng)/ 3}")
+}
