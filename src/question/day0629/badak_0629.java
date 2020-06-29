df4142c955aa7152162ec90c445c0011a8864891
eunbok <eunbok4@naver.com>
2020-06-29 15:54:44 +0900

    badak_0629
    badak_0629

---
+++ src/question/day0629/badak_0629.java
@@ -0,0 +1,18 @@
+package homework.day0629;
+
+public class badak_0629 {
+
+	public static void main(String[] args) {
+		int kor = 90;
+		int mat = 80;
+		int eng = 100;
+		int sum = kor+mat+eng;
+		int avg = sum/3;
+		
+		System.out.println("kor "+kor);
+		System.out.println("mat "+mat);
+		System.out.println("eng "+eng);
+		System.out.println("sum "+sum);
+		System.out.println("avg "+avg);
+	}
+}
