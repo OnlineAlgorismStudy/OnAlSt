cb361ea381643d2b0ef57bc35a51844fe8421f42
JoungSik <tjstlr2010@gmail.com>
2020-06-30 12:48:17 +0900

    Add files via upload

---
+++ src/question/day0701/joungsik_0701.js
@@ -0,0 +1,5 @@
+const [yard, inch] = [91.44, 2.54];
+const [num1, num2] = [2.1, 10.5];
+
+console.log(`${num1}yd = ${(num1 * yard).toFixed(1)}cm`);
+console.log(`${num2}in = ${(num2 * inch).toFixed(1)}cm`);
