787f0552ba43994b4f681e6ea49f56ac9da32068
eunbok <eunbok4@naver.com>
2020-06-30 15:51:17 +0900

    hello world
    hello world

---
+++ src/question/day0630/badak_0630.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class badak_0630 {
+	public static void main(String args[]) {
+		int a = 45;
+		int b = 9;
+		System.out.println("55 - 10 = " + a);
+		System.out.println("2008 - 1999 = " + b);
+	}
+}
