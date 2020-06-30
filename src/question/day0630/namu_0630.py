a189e7e815bf0a99db27ed63a44aee848bc4d6b4
kimxoals <66377873+kimxoals@users.noreply.github.com>
2020-06-30 20:55:30 +0900

    Update namu_0630.py

--- src/question/day0630/namu_0630.py
+++ src/question/day0630/namu_0630.py
@@ -1,3 +1,3 @@
-num1 = int(input("num1: "))
-num2 = int(input("num2: "))
+num1, num2, num3, num4 = 55, 10, 2008, 1999
 print(f'{num1} - {num2} = {num1-num2}')
+print(f'{num3} - {num4} = {num3-num4}')
