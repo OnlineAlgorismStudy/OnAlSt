a8bce8dd1384e5dcd475d66a7e74d549dfa90037
금강누나 <52662748+itkk0306@users.noreply.github.com>
2020-06-30 21:59:11 +0900

    Add files via upload

---
+++ src/question/day0630/goe_0630.java
@@ -0,0 +1,10 @@
+public class goe_0630 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 2000;
+
+        System.out.println( "55 - 10  = " + ((a*5+5) - a) );
+        System.out.println( "2008 - 1999 = " + ((b+8) - (b-1)) );
+    }
+}
