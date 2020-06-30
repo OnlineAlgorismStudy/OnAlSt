855b7b9250c29939496584098c109a6d657b52b0
kimxoals <66377873+kimxoals@users.noreply.github.com>
2020-06-30 23:43:37 +0900

    Update namu_0630.py

--- src/question/day0630/namu_0630.py
+++ src/question/day0630/namu_0630.py
@@ -1,3 +1,4 @@
-num1, num2, num3, num4 = 55, 10, 2008, 1999
-print(f'{num1} - {num2} = {num1-num2}')
-print(f'{num3} - {num4} = {num3-num4}')
+num1, num2 = 45, 9
+print(f'55 - 10 = {num1}')
+print(f'2008 - 1999 = {num2}')
+
