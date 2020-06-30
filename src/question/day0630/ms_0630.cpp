8871d9e97f7dca1eebb92be9615cd25356327bbd
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-06-30 12:49:50 +0900

    algorithm 2

---
+++ src/question/day0630/ms_0630.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 55, y = 10;
+	
+	cout << x << " - " << y << " = " << (x - y) << endl;
+	
+	x = 2008, y = 1999;
+	cout << x << " - " << y << " = " << (x - y) << endl;
+
+	return 0;
+}
