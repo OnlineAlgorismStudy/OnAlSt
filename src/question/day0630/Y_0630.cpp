a2cb0d3b152049972f0a873a6f4522762833cb8e
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-06-30 22:41:18 +0900

    2020.06.30 upload algo by Y
    2020.06.30 upload algo by Y

---
+++ src/question/day0630/Y_0630.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1 = 55, num2 = 10;
+    int num3 = 2008, num4 = 1999;
+
+    printf("%d - %d = %d\n", num1, num2, num1 - num2);
+    printf("%d - %d = %d\n", num3, num4, num3 - num4);
+	return 0;
+}
