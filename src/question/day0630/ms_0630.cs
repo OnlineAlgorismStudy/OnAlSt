e4448589173a1e3555d092853d0230138d1c1629
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-06-30 12:51:35 +0900

    algorithm 2

---
+++ src/question/day0630/ms_0630.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algorithm_02
+{
+    class ms_0630
+    {
+        static void Main(string[] args)
+        {
+            var x = 55;
+            var y = 10;
+            Console.WriteLine($"{x} - {y} = {x-y}");
+            x = 2008;
+            y = 1999;
+            Console.WriteLine($"{x} - {y} = {x-y}");
+        }
+    }
+}
