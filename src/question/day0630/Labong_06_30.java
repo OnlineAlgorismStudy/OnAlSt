0ca18978283a15b93ce14f7a0960d4e7636b9807
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-06-30 12:36:39 +0900

    뿡라봉 06_30

---
+++ src/question/day0630/Labong_06_30.java
@@ -0,0 +1,17 @@
+
+public class Labong_06_30 {
+
+	public static void main(String[] args) {
+		
+		int month = 55;
+		int year = 2008;
+		
+		System.out.println(month + " - " + 10 + " = " + (month - 10) );
+		System.out.println(year + " - " + 1999 + " = " + (year -1999));
+		
+		
+		
+		
+	}
+
+}
