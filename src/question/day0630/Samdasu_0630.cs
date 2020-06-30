d0a2e1c4398743601238a3aa791ba7f6ee9c3212
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-06-30 21:04:46 +0900

    삼다수 0630

---
+++ src/question/day0630/Samdasu_0630.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+//두개의 정수형 변수를 선언하고 값을 대입하여 아래와 같이 출력되는 프로그램을 작성하시오.
+namespace Samdasu_0630
+{
+    class Samdasu_0630
+    {
+        static void Main(string[] args)
+        {
+            int a = 55;
+            int b = 2008;
+            Console.WriteLine("{0} - 10 = {1}", a, a-10);
+            Console.WriteLine("{0} - 1999 = {1}", b, b - 1999);
+        }
+    }
+}
