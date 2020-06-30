a4524670460971866cb61bfe407376f87109fb86
Lampard <45754698+JWooni@users.noreply.github.com>
2020-06-30 12:35:01 +0900

    0630

---
+++ src/question/day0630/bangJjo_0630.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class bangJjo_0630 {
+
+	public static void main(String[] args) {
+
+		int a = 55 - 10;
+		int b = 2008 - 1999;
+		
+		System.out.println("55 - 10 = "+a);
+		System.out.println("2008 - 1999 = "+b);
+	
+	} // main
+
+} // class
