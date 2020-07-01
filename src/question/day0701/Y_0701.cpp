bc6ae7a8d721aab2cbd886bcc6c8007602b568ba
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-07-01 18:41:41 +0900

    2020.07.01 : upload algo by Y

---
+++ src/question/day0701/Y_0701.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double yard = 91.44;
+	double inch = 2.54;
+
+	printf("2.1yd = %.1fcm\n", yard * 2.1);
+	printf("10.5in = %.1fcm\n", inch * 10.5);
+}
