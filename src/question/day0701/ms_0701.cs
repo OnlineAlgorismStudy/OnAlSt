446d0c15ea67a0f38352b4d810ee610e3cbfc99d
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-01 14:51:55 +0900

    algorithm 3

---
+++ src/question/day0701/ms_0701.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace algorithm_03
+{   
+    class ms_0701
+    {
+        const float YD = 91.44f;
+        const float IN = 2.54f;
+
+        static void Main(string[] args)
+        {
+            float x = 2.1f;
+            float y = 10.5f;
+
+            Console.WriteLine($" {x}yd = {(x*YD):.0}cm");
+            Console.WriteLine($"{y}in =  {(y*IN):.0}cm");
+        }
+    }
+}
