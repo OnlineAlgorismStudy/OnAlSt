2db2fa14ea880633d3151af647fddd00dd35bef6
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-07-01 17:47:55 +0900

    BR200701_java

---
+++ src/question/day0701/BR_0701.java
@@ -0,0 +1,13 @@
+
+public class BR_0701 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double yd = 91.44;
+		double in = 2.54;
+		
+		System.out.println("2.1yd = " + String.format("%.1f", 2.1*yd) + "cm");
+		System.out.println("10.5in = " + String.format("%.1f", 10.5*in) + "cm");
+	}
+
+}
