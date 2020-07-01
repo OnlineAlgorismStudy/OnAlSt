4b06c8db0441a47c6b4747fe3c3237d18a383dfc
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-07-01 17:36:37 +0900

    BR200630_java

---
+++ src/question/day0630/BR_0630.java
@@ -0,0 +1,18 @@
+
+public class BR_0630 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 0;
+		
+		a = 55;
+		b = 10;
+		System.out.println(a + " - " + b + " = " + (a-b));
+		
+		a = 2008;
+		b = 1999;
+		System.out.println(a + " - " + b + " = " + (a-b));
+	}
+
+}
