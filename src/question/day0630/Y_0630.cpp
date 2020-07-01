dc3bdfb2e96f633a53f2535257419cfed1fc55d9
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-07-01 18:37:01 +0900

    Update Y_0630.cpp

--- src/question/day0630/Y_0630.cpp
+++ src/question/day0630/Y_0630.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int num1 = 55, num2 = 10;
-    int num3 = 2008, num4 = 1999;
-
+    int num1 = 55, num2 = 10;
     printf("%d - %d = %d\n", num1, num2, num1 - num2);
-    printf("%d - %d = %d\n", num3, num4, num3 - num4);
-	return 0;
+	
+    num1 = 2008, num2 = 1999;
+    printf("%d - %d = %d\n", num1, num2, num1 - num2);
+    return 0;
 }
