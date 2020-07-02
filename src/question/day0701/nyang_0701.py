8182635c45005b786422906d58f416bcbf9f547b
JiyeonCha <66763635+JiyeonCha@users.noreply.github.com>
2020-07-02 13:48:01 +0200

    냥_0701_파이썬

---
+++ src/question/day0701/nyang_0701.py
@@ -0,0 +1,9 @@
+#2.1yd = 192.0cm
+#10.5in = 26.7cm
+
+yd_to_cm=lambda yd:yd*91.44
+print '2.1yd =', round(yd_to_cm (2.1),1)
+
+inch_to_cm=lambda inches:inches*2.54
+
+print '10.5in =', round(inch_to_cm(10.5),1)
