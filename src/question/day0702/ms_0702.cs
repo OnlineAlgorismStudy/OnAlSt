988b9fa234f770b81fd20917b8e53dd7c8868884
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-02 12:51:53 +0900

    algorithm 4

---
+++ src/question/day0702/ms_0702.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace algorithm_04
+{
+    class ms_0702
+    {
+        static void Main(string[] args)
+        {
+            const double YD = 91.44;
+            
+            Console.Write("yard? ");
+            var input = Console.ReadLine();
+            double yard = Convert.ToDouble(input);
+
+            Console.WriteLine($"{yard}yard = {yard*YD:.0}cm");
+        }
+    }
+}
