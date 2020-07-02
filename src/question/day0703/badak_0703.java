4b277f2dccd68d845b1ce9590f543289aa96d5c8
eunbok <eunbok4@naver.com>
2020-07-02 21:55:25 +0900

    야근이 싫은 바닥
    바닥

---
+++ src/question/day0703/badak_0703.java
@@ -0,0 +1,23 @@
+package homework;
+
+import java.util.Scanner;
+
+public class badak_0703 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String a[] = sc.nextLine().split(" ");
+		
+		int b = Integer.parseInt(a[0])+5;
+		int c = Integer.parseInt(a[1])*2;
+		
+		System.out.println("width = "+b);
+		System.out.println("length = "+c);
+		System.out.println("area = "+b*c);
+	
+	}
+
+}
+
