9ae73298654c06fc6b63a20c4384bca300194f24
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-07-02 20:44:53 +0900

    삼다수 0702

---
+++ src/question/day0702/samdasu_0702.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samdasu_0702
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yard = 91.44;
+            
+
+            Console.WriteLine("yard?: ");
+            string input = Console.ReadLine();
+
+            var cm = (Convert.ToDouble(input) * 91.44).ToString();
+            Console.WriteLine("{0:0.0}yard = {1:0.0}cm", input, cm);
+
+        }
+    }
+}
