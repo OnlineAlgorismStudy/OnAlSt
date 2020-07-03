0199fa52a45d381dec16f6b8770ed781e15928fd
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-07-03 23:08:26 +0900

    2020.07.03 : upload algo by Y

---
+++ src/question/day0703/Y_0703.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		int width, length;
+    scanf("%d %d", &width, &length);
+
+    width += 5;
+    length *= 2;
+    
+    printf("width = %d\n", width);
+    printf("length = %d\n", length);
+    printf("area = %d\n", width * length);
+		return 0;
+}
