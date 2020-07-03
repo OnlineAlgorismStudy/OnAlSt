63971f2579e6c2926fd3622100178b98a7e1a679
eunbok <eunbok4@naver.com>
2020-07-03 14:42:35 +0900

    Update badak_0703.java
    주석제거

--- src/question/day0703/badak_0703.java
+++ src/question/day0703/badak_0703.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class badak_0703 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
 		String a[] = sc.nextLine().split(" ");
