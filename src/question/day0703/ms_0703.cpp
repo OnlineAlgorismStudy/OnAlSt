44a110f44073dad3442e8ca23dafc9b9ce8edbfd
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-03 09:11:05 +0900

    algorithm 5

---
+++ src/question/day0703/ms_0703.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int width;
+	int length;
+
+	cin >> width >> length;
+
+	width += 5;
+	length *= 2;
+
+	cout << "width = " << width << endl;
+	cout << "length = " << length << endl;
+	cout << "area = " << width * length << endl;
+
+	return 0;
+}
