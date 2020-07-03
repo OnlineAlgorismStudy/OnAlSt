8200497d7884ad2a0ad5ffd3784b66ea60023f75
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-03 09:25:24 +0900

    민석스

---
+++ src/question/day0703/ms_0703.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algorithm_05
+{
+    class ms_0703
+    {
+        static void Main(string[] args)
+        {
+            string[] area = Console.ReadLine().Split(new char[] {' '});
+            var width = Convert.ToInt32(area[0]) + 5;
+            var length = Convert.ToInt32(area[1]) * 2;
+
+            Console.WriteLine($"width = {width}\nlength = {length}");
+            Console.WriteLine($"area = {width * length}");
+        }
+    }
+}
