156497d849d77796a7c01f4e13453fea04ff5704
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-07-04 13:22:24 +0900

    2020.07.04 : upload also by Y

---
+++ src/question/day0706/Y_0706.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int year;
+    scanf("%d", &year);
+
+    if(year%400 == 0 || (year%4 == 0 && year%100 != 0)) {
+        printf("leap year\n");
+    } else {
+        printf("common year\n");
+    }
+	return 0;
+}
