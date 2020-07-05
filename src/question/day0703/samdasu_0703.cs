6b0abe84b27e9c9f2aa3c1db7476647fca5713ec
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-07-05 19:00:26 +0900

    삼다수0703

---
+++ src/question/day0703/samdasu_0703.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samdasu_0703
+{
+    class samdasu_0703
+    {
+        static void Main(string[] args)
+        {
+            int ver;
+            int hor;
+
+            ver = Convert.ToInt32(Console.ReadLine());
+            ver +=5;
+            hor = Convert.ToInt32(Console.ReadLine());            
+            hor *= 2;
+
+            Console.WriteLine("width = {0}\nlength = {1}\narea = {2}", ver, hor, ver * hor);
+
+
+        }
+    }
+}
