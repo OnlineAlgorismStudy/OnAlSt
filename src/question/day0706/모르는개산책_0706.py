f97a4cde93593307dd2793595d7b0ef864a8e8e7
inji728 <67571622+inji728@users.noreply.github.com>
2020-07-05 23:25:03 -0400

    Add files via upload

--- src/question/day0706/모르는개산책_0706.py
+++ src/question/day0706/모르는개산책_0706.py
@@ -8,7 +8,11 @@
 
 year = int(input(''))
 
-if year%4 == 0:
+if year % 400 == 0:
     print('leap year')
+    
+elif year % 4 == 0 or year % 100 != 0:
+    print('leap year')
+
 else:
     print('common year')
