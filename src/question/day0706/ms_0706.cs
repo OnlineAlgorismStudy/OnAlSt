162b829bfe514f15c0da98b50b999264d71b8e2d
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-06 14:37:48 +0900

    민석스 :D

---
+++ src/question/day0706/ms_0706.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algorithm_06
+{
+    class ms_0706
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if((year%4 == 0 && year%100 != 0) || year%400 == 0)
+                Console.WriteLine("leap year");
+            else 
+                Console.WriteLine("common year");
+        }
+    }
+}
