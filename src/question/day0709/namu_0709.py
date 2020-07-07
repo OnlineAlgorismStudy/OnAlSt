674247e6dceb3981befe6862c4809cd656e39a3a
Kevin <66377873+kimxoals@users.noreply.github.com>
2020-07-07 13:46:52 +0900

    나무

---
+++ src/question/day0709/namu_0709.py
@@ -0,0 +1,7 @@
+end = False
+while not end:
+    base = float(input("Base = "))
+    height = float(input("Height = "))
+    width = float(input("Triangle width = "))
+    if input("Continue? ").lower() != "y":
+        end = True
