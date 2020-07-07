7da1f0dc0cfde82fdff0d5001ce4065ee8b1d63f
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-07 13:17:03 +0900

    민석스

---
+++ src/question/day0707/ms_0707.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace algorithm_07
+{
+    class ms_0707
+    {
+        static void Main(string[] args)
+        {
+            string[] animal = { "dog", "cat", "chick" };
+
+            Console.Write("Number? ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            if(1 <= num && num <= 3)
+                Console.WriteLine(animal[num-1]);
+            else
+                Console.WriteLine("I don't know");
+        }
+    }
+}
