f0a4a8385e9afd1d808801c167011e9f7f63f4b4
Kevin <66377873+kimxoals@users.noreply.github.com>
2020-07-07 19:15:16 +0900

    Update namu_0709.py

--- src/question/day0709/namu_0709.py
+++ src/question/day0709/namu_0709.py
@@ -2,6 +2,6 @@ end = False
 while not end:
     base = float(input("Base = "))
     height = float(input("Height = "))
-    width = float(input("Triangle width = "))
+    print("Triangle width = ",base * height /2)
     if input("Continue? ").lower() != "y":
         end = True
