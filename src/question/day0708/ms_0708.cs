f774d496b5af4e0ae5843dd763dbd3e204d2907b
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-08 14:11:42 +0900

    민석스

---
+++ src/question/day0708/ms_0708.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace algorithm_08
+{
+    class ms_0708
+    {
+        public static readonly string[] state = { "Korea", "USA", "Japen", "China" };
+        public static readonly string[] capital = { "Seoul", "Washington", "Tokyo", "Beijing" };
+
+        static void Main(string[] args)
+        {
+            
+            while(true) {
+                for(var i = 0; i < 4; i++)
+                    Console.WriteLine($"{i+1}. {state[i]}");
+
+                Console.Write("number? ");
+                int cnt = Convert.ToInt32(Console.ReadLine());
+
+                if(1 <= cnt && cnt <= 4) 
+                    Console.WriteLine($"\n{capital[cnt-1]}\n");
+                else {
+                    Console.WriteLine("\nnone\n");
+                    break;
+                }
+            }
+        }
+    }
+}
