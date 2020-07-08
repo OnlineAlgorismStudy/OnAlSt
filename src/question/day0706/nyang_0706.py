e53628c00aad61363d741e33fb6a82e903114a62
JiyeonCha <66763635+JiyeonCha@users.noreply.github.com>
2020-07-08 22:36:04 +0200

    냥_0706_파이썬
    수정했어욤...

--- src/question/day0706/nyang_0706.py
+++ src/question/day0706/nyang_0706.py
@@ -1,10 +1,10 @@
 def leapyr(n):
     if n % 400 == 0:
         return "leap year"
-    if n % 100 == 0:
-        return "common year"
-    if n % 4 == 0:
+
+    if n % 4 == 0 and n % 100 != 0:
         return "leap year"
+
     else:
         return "common year"
 print leapyr(2008)
