82b489ef5d55606b905c3a019b8a40b168e8c724
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-07-08 23:44:53 +0900

    Update Jjingu_0710.py

--- src/question/day0710/Jjingu_0710.py
+++ src/question/day0710/Jjingu_0710.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def PorF(n,score):
     score_re = np.float32(score.split(' '))
-    avg = rount(score_re.sum()/n,1)
+    avg = score_re.sum()/n
     print('avg :',avg)
     if avg>=80:
         print("pass")
@@ -18,6 +18,17 @@ def PorF(n,score):
         print("fail")
 
 n = int(input())
+while n>10 or n<0 :
+    print('Wrong Number of subject. The Number of subject should be lower than 11')
+    n = int(input())
+    
 score = input()
+while len(score.split(' '))!=n or np.max(np.float32(score.split(' ')))>100 or np.min(np.float32(score.split(' ')))<0:
+    if len(score.split(' '))!=n:
+        print('The number of subject is not same with the number set already. repeat again')
+    elif np.max(np.float32(score.split(' ')))>100 or np.min(np.float32(score.split(' ')))<0:
+        print('The score should be in the range between 0 and 100')
+        
+    score = input()
 
 PorF(n,score)
