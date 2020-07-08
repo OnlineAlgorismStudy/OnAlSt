028562f6f4421de3dd79ecc46296ce5df3f753ca
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-07-08 22:59:52 +0900

    찐구_0710.py

---
+++ src/question/day0710/Jjingu_0710.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 22:48:36 2020
+
+@author: hansong
+"""
+
+#아대숲 찐구 0710 문제풀이 
+import numpy as np
+
+def PorF(n,score):
+    score_re = np.float32(score.split(' '))
+    avg = score_re.sum()/n
+    print('avg :',avg)
+    if avg>=80:
+        print("pass")
+    else:
+        print("fail")
+
+n = int(input())
+score = input()
+
+PorF(n,score)
