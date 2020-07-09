a8fe54daf2a8b75dc3a1d04fbf1dcf3ab900e8d0
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-09 13:20:03 +0900

    민석스

---
+++ src/question/day0709/ms_0709.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace algorithm_09
+{
+    class ms_0709
+    {
+        static void Main(string[] args)
+        { 
+            char cnd;
+            
+            do {
+                Console.Write("Base = ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                
+                Console.Write("Height = ");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Triangle width = {width*height/2.0:.0}");
+                
+                Console.Write("Continue? ");
+                cnd = Convert.ToChar(Console.ReadLine());
+            } while(cnd == 'Y' || cnd == 'y');
+        }
+    }
+}
