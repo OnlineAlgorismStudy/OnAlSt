9c55efe1299ad0ee2ae6d45abd2dec33c373a067
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-10 15:18:02 +0900

    민석스

---
+++ src/question/day0710/ms_0710.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace algorithm_10
+{
+    class ms_0710
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if(n <= 10) {
+                int sum = 0;
+                string info = Console.ReadLine();
+                string[] subjects = info.Split(' ');
+
+                foreach (var subject in subjects) {
+                    sum += Convert.ToInt32(subject);
+                }
+
+                double avg = sum / (double) n;
+                Console.WriteLine($"avg : {avg:.0}");
+                Console.WriteLine((avg >= 80)? "pass" : "fail");
+            }
+        }
+    }
+}
