adaeb4f43f94f151ec05ece03e72dd0aedd20bc7
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-07-10 22:29:17 +0900

    삼다수 0708

---
+++ src/question/day0708/Samdasu_0708.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0708
+{
+    /*
+    enum capital
+    {
+        Seoul = 1, Washington, Tokyo, Beijing    
+    }
+    */
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.Write("1.Korea\n2.USA\n3.Japan\n4.China\n");
+            Console.Write("Number? ");
+            int x = Convert.ToInt32(Console.ReadLine());            
+            Console.Write("\n");
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Seoul");
+                    break;
+                case 2:
+                    Console.WriteLine("Washington");
+                    break;
+                case 3:
+                    Console.WriteLine("Tokyo");
+                    break;
+                case 4:
+                    Console.WriteLine("Beijing");
+                    break;
+                default:
+                    Console.WriteLine("none");
+                    break;
+            }            
+        }
+    }
+}
