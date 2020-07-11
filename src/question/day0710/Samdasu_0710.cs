2cd6ffe4468cbb5baa47a0388aaa2d4303517986
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-07-11 17:18:29 +0900

    삼다수 0710

---
+++ src/question/day0710/Samdasu_0710.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Samdasu_0710
+{
+    class Samdasu_0710
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            if (n <= 10)
+            {
+                string subSum = Console.ReadLine();
+                string[] sub = subSum.Split(' ');
+                
+
+                for(int i = 0; i<sub.Length; i++)
+                {
+                    sum += Convert.ToInt32(sub[i]);
+                }
+            }
+            else { return; }
+
+            double avg = (double)sum / (double)n;
+
+            Console.WriteLine("avg : {0:F1}", avg);
+            if(avg >= 80)
+            {
+                Console.WriteLine("pass");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+
+        }
+    }
+}
