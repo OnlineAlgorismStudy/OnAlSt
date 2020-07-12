4dcfc6ca6737e918bf2edf2395f8406c06f88856
JiyeonCha <66763635+JiyeonCha@users.noreply.github.com>
2020-07-12 13:15:11 +0200

    냥_0710_파이썬
    뉴

--- src/question/day0710/nyang_0710.py
+++ src/question/day0710/nyang_0710.py
@@ -1,19 +1,30 @@
-def average():
-    s = 0
-    n=int(input("과목의 갯수: "))
+def average():   
+    
+    while True:
+     
+        try:
+            s = 0
+            n = int(input("과목의 갯수: "))
+            if n < 1 or n > 10:
+                raise ValueError 
+        
+        except ValueError:
+            print("1-10까지의 숫자를 입력해주세요.")
+            break
+            
 
-    for x in range(1,n+1):
-        a=float(input("%d번째 과목 점수: "%(x)))
-        s+=a
+        for x in range(1,n+1):
+            a=float(input("%d번째 과목 점수: "%(x)))
+            s+=a
 
 
-    print("avg : %.2f "%(round(s/n,1)))
+        print("avg : %.2f "%(round(s/n,1)))
 
 
-    if s/n >= 80:
-        print("pass")
+        if s/n >= 80:
+            print("pass")
 
-    else:
-        print("fail")
+        else:
+            print("fail")
 
 average()
