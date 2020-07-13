20358024f08b84afba6e6f52ecb5ed39ace5b710
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-13 10:02:45 +0900

    민석스

---
+++ src/question/day0713/ms_0713.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algorithm_11
+{
+    class ms_0713
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i < 5; i++) {
+                for(int j = 1; j < 6; j++) {
+                    Console.Write($"{i} * {j} = {i*j, 2:#0}   ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
