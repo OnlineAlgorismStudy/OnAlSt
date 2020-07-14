9c2c97ac8d44b66b65076bd86c1b12f9a9b05d9f
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-07-14 23:51:34 +0900

    라봉
    라봉

---
+++ src/question/day0715/Labong_07_15.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Labong_07_15 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("ют╥б: ");
+		int num = sc.nextInt();
+		int num2 = num*2;
+		String result = "";
+		
+		for(int i = 0; i < num; i++) {
+			for(int k = i; k < num ; k++) {
+				result += "*";
+							
+			}
+			result += "\n";		
+		} // for
+		
+		for(int a = 0; a < num; a++) {
+			for(int b = 0; b <= a; b++) {
+				result += "*";
+			}
+			result += "\n";
+		}
+		
+		System.out.println(result);
+		
+	} // main
+
+}
