d4576d203438e51ee52214c4d73d09dbac894f5c
ImPaper <67574255+ImPaper@users.noreply.github.com>
2020-07-15 18:19:23 +0900

    3다수

---
+++ src/question/day0713/samdasu_0713.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samsasu_0713
+{
+    class samdasu_0713
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i<5; i++)
+            {
+                for(int j =1; j<6; j++)
+                {
+                    Console.Write("{0} * {1} =  {2}   ", i, j, i * j);
+                    if(j == 5)
+                    {
+                        Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
