2a0c6aa232daa3b6fbcf5d7fa41d6a7bbb6e45f0
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-17 23:58:54 +0900

    민석스

---
+++ src/question/day0717/ms_0717.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace algorithm_15
+{
+    class ms_0717
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
+
+            for(int i = n; i > 0; i--, j++) {
+                Console.Write(new String(' ', j));
+                Console.WriteLine(new String('*', 2*i-1));
+            }
+        }
+    }
+}
