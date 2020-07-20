99afa6beddc1eb678ef7824e3831f727064ddafb
Lampard <45754698+JWooni@users.noreply.github.com>
2020-07-20 22:15:43 +0900

    방쬬

---
+++ src/question/day0630/bangJjo_0630.js
@@ -0,0 +1,10 @@
+/**
+ * http://usejsdoc.org/
+ */
+/*jshint esversion: 6 */
+
+let [a, b] = [55, 10];
+console.log(`${a} - ${b} = ${a - b}`);
+
+let [c, d] = [2008, 1999];
+console.log(`${c} - ${d} = ${c - d}`);
