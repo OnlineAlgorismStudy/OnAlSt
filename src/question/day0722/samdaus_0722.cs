efdad1fca56710be73684d507c4a6ec7d41c5916
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-07-23 02:02:13 +0900

    333다수

---
+++ src/question/day0722/samdaus_0722.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace samdasu_0722
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i =1; i<=n; i++)
+            {
+                for(int j =1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for(int i =n-1; i>=1; i--)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+           /* for(int i = 0; i<((n*2)-1); i++)
+            {
+                for(int j= i; j<n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            */
+        }
+    }
+}
