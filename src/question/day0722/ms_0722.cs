3d7b1ee27710f731bf90c5c3dd09967ebf5581b0
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-22 17:34:33 +0900

    민석스

---
+++ src/question/day0722/ms_0722.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace algorithm_18
+{
+    class ms_0722
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = -(n-1); i <= (n-1); i++) {
+                // 2중 for 문
+                for(int j = 0; j < (n - Math.Abs(i)); j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+                // 단일 for 문
+                //Console.WriteLine(new String('*', n - Math.Abs(i)));
+            }
+        }
+    }
+}
