f93f32b33104ae4f5bbed36c8c57eeefb601423f
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-07-24 15:54:34 +0900

    Add files via upload

---
+++ src/question/day0731/BR_0731.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class BR_0731 {
+
+	public static void main(String[] args) {
+		/* **************************************************
+		 * 문제.
+		 * 원하는 구구단의 범위를 입력받아 해당 구간의 구구단을 출력하는 프로그램을 작성하시오.
+		 * 
+		 * 
+		 * <처리조건>
+		 * (1) 구간의 처음과 끝을 입력받는다.
+		 * (2) 입력된 구간은 반드시 처음 입력 값이 끝의 입력 값보다 작아야 하는 것은 아니다.
+		 * 
+		 * 즉 입력된 구간의 범위는 증가하거나 감소하는 순서 그대로 출력되어야 한다.
+		 * 
+		 * - 입력 형식
+		 * 구구단의 시작 범위 s,와 끝 범위 e를 입력받는다.(s와 e는 2부터 9사이의 정수)
+		 * 하나의 결과가 출력되면 프로그램을 종료한다.
+		 * 
+		 * - 출력 형식
+		 * 시작 범위와 끝 범위사이의 구구단을 출력하되 모든 값과 부호 사이는 공백으로 구분하여 아래 출력 예와 같이 줄을 맞추어 출력해야 한다.
+		 * 구구단 사이는 3개의 공백으로 구분한다.
+		 * 데이터의 크기가 주어진 범위를 벗어날 경우는 "INPUT ERROR!"를 출력하고 s와 e를 다시 입력받는다.​​​
+		 * 
+		 * 입력 예.
+		 * ===========================
+		 * 4 3
+		 * ===========================
+		 * 
+		 * 출력 예.
+		 * ===========================
+		 * 4 * 1 =  4   3 * 1 =  3
+		 * 4 * 2 =  8   3 * 2 =  6
+		 * 4 * 3 = 12   3 * 3 =  9
+		 * 4 * 4 = 16   3 * 4 = 12
+		 * 4 * 5 = 20   3 * 5 = 15
+		 * 4 * 6 = 24   3 * 6 = 18
+		 * 4 * 7 = 28   3 * 7 = 21
+		 * 4 * 8 = 32   3 * 8 = 24
+		 * 4 * 9 = 36   3 * 9 = 27
+		 * ===========================
+		 * **************************************************/
+
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		int e = sc.nextInt();
+		int[] range = fnRearrange(s, e);
+		
+		while (range[1] < 2 || range[0] > 9) {
+			System.out.println("INPUT_ERROR!");
+			range = fnRearrange(s = sc.nextInt(), e = sc.nextInt());
+		}
+		sc.close();
+		
+		String table = "";
+		for (int i = 1; i <= 9; i++) {
+			int num = s;
+			for (int j = 0; j <= range[1]-range[0]; j++) {
+				table += num + " * " + i + " = " + String.format("%2d", i*num);
+				if (num != e) table += String.format("%3s", " ");
+				if (s == range[0]) num++;
+				else num--;
+			}
+			table += "\n";
+		}
+		System.out.println(table);
+		
+	}
+	
+	
+	private static int[] fnRearrange(int from, int to) {
+		int[] results = new int[2];
+		if (to > from) {
+			results[0] = from;
+			results[1] = to;
+		} else {
+			results[0] = to;
+			results[1] = from;
+		}
+		
+		return results;
+	}
+}
