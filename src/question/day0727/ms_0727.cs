9df17650c7ff0957b34f94d09e8b1fbb3bd0d16b
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-07-26 08:34:58 +0900

    민석스

---
+++ src/question/day0727/ms_0727.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace algorithm_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = n-1; i >= 0; i--) {                
+                for(int j = 1; j <= i; j++) {
+                    Console.Write("  ");
+                }
+
+                int cnt = 1;
+                for(int j = 1; j <= n-i; j++) {
+                    Console.Write(cnt++);
+
+                    if(j != n-i)
+                        Console.Write(' ');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
