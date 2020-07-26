f307d945a299bf579f2268630c64d4c840df4248
Kevin <66377873+kimxoals@users.noreply.github.com>
2020-07-27 01:38:29 +0900

    Update namu_0728.py

--- src/question/day0728/namu_0728.py
+++ src/question/day0728/namu_0728.py
@@ -2,14 +2,10 @@ n = int(input())
 list_int = \
     [str(x) + ' ' if x < 10 else str(x) for x in range(1, n * (n + 1) // 2 + 1)]
 
-
-k = n * 3 - 1
 p = 0
 for i in range(n):
     if n < 5:
-        print((''.join(list_int[p:p + n - i])).rjust(k - 2))
+        print((''.join(list_int[p:p + n - i])).rjust(2 * n))
     else:
-        print((' '.join(list_int[p:p + n - i])).rjust(k))
+        print((' '.join(list_int[p:p + n - i])).rjust(n * 3 - 1))
     p += n - i
-
-
