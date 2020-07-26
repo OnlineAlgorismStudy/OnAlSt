1bb69868efff478e81771dfbd880af6ec6f4e698
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-07-26 23:46:30 +0900

    찐구 0728.py

---
+++ src/question/day0728/Jjingu_0728.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 22:51:54 2020
+
+@author: hanso
+"""
+# 아대숲 찐구 0728 풀이
+def num_tri2(num):
+    k = 1
+    for i in reversed(range(1,num+1)):
+        num_str = ""
+        for j in range(0,i):
+            num_str += " "+str(k)
+            k = k+1
+        
+        print(num_str.rjust(num*2))
+        
+
+num = int(input())
+num_tri2(num)
+
