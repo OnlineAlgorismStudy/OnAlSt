02aecfa2b2a80a92b064d0247afb8a4d8aaa3e72
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-02 13:25:03 +0900

    3다수

---
+++ src/question/day0730/samdasu0730.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace samdasu0730
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int num = 1;
+
+            for(int i = 0; i<n; i++)
+            {
+                for(int j = 0; j<n; j++)
+                {
+                    Console.Write("{0} ", num % 10);
+                    num += 2;
+                }
+                Console.WriteLine("");
+            }
+            
+        }
+    }
+}
