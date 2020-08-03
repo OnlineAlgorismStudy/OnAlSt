16b36fe706b4afd67111e7940ba10ea6aebae4b5
Lampard <45754698+JWooni@users.noreply.github.com>
2020-08-03 19:50:09 +0900

    방쬬

---
+++ src/question/month08/day0804/bangJjo_0804.java
@@ -0,0 +1,25 @@
+package question.month08.day0804;
+
+import java.util.Scanner;
+
+
+public class bangJjo_0804 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		char[] c = new char[10];
+		
+		for(int i = 0; i < c.length; i++) {
+			String input = sc.next();
+			c[i] = input.charAt(0);
+		} // end for
+		
+		sc.close();
+		
+		System.out.print(c[0] + " " + c[3] + " " + c[6]);
+		
+	} // main
+
+} // class
