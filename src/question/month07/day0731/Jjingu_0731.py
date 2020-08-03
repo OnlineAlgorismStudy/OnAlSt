7a7364a787cb30f5a96b365c5e16fb6a21154026
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-08-04 00:08:56 +0900

    Update Jjingu_0731.py
    범위 모두를 표현하도록 수정함

--- src/question/month07/day0731/Jjingu_0731.py
+++ src/question/month07/day0731/Jjingu_0731.py
@@ -8,8 +8,20 @@ import numpy as np
 
 def product_express(num1,num2):
     if (num1>1 and num1<10) and (num2>1 and num2<10):
-        for i in range(1,10):
-            print(f"{num1} * {i} =  {num1*i:>2}   {num2} * {i} =  {num2*i:>2}")
+        for count in range(1,10):
+            if num1> num2:
+                for i in reversed(range(num2,num1+1)):
+                    if i == num2:
+                        print(f"{i} * {count} =  {i*count:>2}",end ='\n')
+                    else:
+                        print(f"{i} * {count} =  {i*count:>2}",end='    ')
+                
+            else:
+                for i in range(num1,num2+1):
+                    if i == num2:
+                        print(f"{i} * {count} =  {i*count:>2}",end ='\n')
+                    else:
+                        print(f"{i} * {count} =  {i*count:>2}",end='    ')
     
     else:
         print("INPUT ERROR!")
@@ -21,5 +33,6 @@ num1 = num_re[0]
 num2 = num_re[1]
 
 product_express(num1, num2)
+
 
 
