87ad0baded1c4652c5f8e1836cb74dec01adf9bc
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-03 17:52:57 +0900

    민석스

---
+++ src/question/month08/day0803/ms_0803.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace algorithm_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input = Console.ReadLine().Split(' ');
+
+            // 10 개의 문자만 처리
+            // 1, 4, 7 번째 문자 출력
+            bool[] onOff = { true, false, false, true, false,
+                             false, true, false, false, false };
+
+            for(int i = 0; i < onOff.Length; i++) {
+                if(onOff[i]) {
+                    Console.Write(input[i] + ' ');
+                }
+            }
+
+            //귀찮을 때
+            //Console.WriteLine($"{input[0]} {input[3]} {input[6]}");
+        }
+    }
+}
