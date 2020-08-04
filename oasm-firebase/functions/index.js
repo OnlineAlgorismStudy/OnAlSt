4617e85f2f49f25e9909cd052e29745dc52960af
JoungSik-MADX <joungsik@madx.company>
2020-08-04 17:21:11 +0900

    [Change] 신규 DB 업데이트

--- oasm-firebase/functions/index.js
+++ oasm-firebase/functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+// firebase emulators:start
+// firebase deploy --only functions
 
 const admin = require("firebase-admin");
 
@@ -9,42 +11,34 @@ const serviceAccount = require("./oasm.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://oasm-889a7.firebaseio.com/",
 });
 
-const db = admin.database();
-const ref = db.ref("users");
+const db = admin.firestore();
 
 exports.message = functions.https.onRequest((request, response) => {
   const data = request.query;
-  console.log(data);
   if (data.files !== "" && data.files !== undefined && data.files !== null) {
     const files = data.files.split(",");
     files.map((file) => {
       const isALST = RegExp("src/question").test(file);
-      if (isALST) {
+      const isQuestion = RegExp(".JPG").test(file);
+      if (isALST && !isQuestion) {
         const names = file.split("/");
-        const date =
-          "2020-" +
-          names[2]
-            .replace("day", "")
-            .replace(/(\d{2})(\d{1,2})/, ($f, $1, $2) =>
-              $f.replace($1, $1 + "-")
-            );
 
-        db.ref("files")
-          .child(names[3].split(".")[0])
-          .set(
-            JSON.parse(
-              JSON.stringify({
-                date: date,
-                extension: names[3].split(".")[1],
-                message: data.message,
-                upload: data.date,
-                user: data.name,
-              })
-            )
-          );
+        const date = `2020-${names[2].replace("month", "")}-${names[3]
+          .replace("day", "")
+          .slice(-2)}`;
+
+        const docRef = db
+          .collection(date)
+          .doc(data.name)
+          .collection("files")
+          .doc(names[4].split(".")[1]);
+
+        docRef.set({
+          name: names[4],
+          upload: data.date,
+        });
       }
     });
   }
