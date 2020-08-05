f2c0c16fca4673166770b0c388643619c1dc7b94
inji728 <67571622+inji728@users.noreply.github.com>
2020-08-06 02:41:48 +0900

    Add files via upload

---
+++ src/question/month08/day0803/모르는개산책_0803.py
@@ -0,0 +1,13 @@
+# 2020 08 03 Algorithm Study Day 28
+# Inji Seo
+
+'''
+10개의 문자를 입력받아서 첫 번째 네 번째 일곱 번째 입력받은 문자를 차례로 출력하는 프로그램을 작성하시오.
+
+입력 예: A B C D E F G H I J
+출력 예: A D G
+'''
+
+mylist = input('').split( )
+
+print(mylist[0],mylist[3],mylist[6])
