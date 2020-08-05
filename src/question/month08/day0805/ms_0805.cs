72ecdb5297304c8179fbedd5774af724b16e747b
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-05 09:49:48 +0900

    민석스

---
+++ src/question/month08/day0805/ms_0805.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace algorithm_28
+{
+    class ms_0805
+    {
+        static void Main(string[] args)
+        {
+            double[] avg = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };
+            string[] input = Console.ReadLine().Split(' ');
+            double sum = 0;
+
+            for(int i = 0; i < 2; i++) {
+                sum += avg[Convert.ToInt32(input[i]) - 1];
+            }
+
+            Console.WriteLine($"{sum:.0}");
+        }
+    }
+}
