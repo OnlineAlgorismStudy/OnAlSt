ee6e051241978aa2f3618b0a9c8c44bd06077b6b
eunbok <eunbok4@naver.com>
2020-08-05 17:27:29 +0900

    바닥
    바닥

--- src/question/month08/day0804/badak_0804.java
+++ src/question/month08/day0804/badak_0804.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 public class badak_0804 {
 
 	/*
-	 * 0803
-
-10개의 문자를 입력받아서 첫 번째 네 번째 일곱 번째 입력받은 문자를 차례로 출력하는 프로그램을 작성하시오.
-
--입력 예
-A B C D E F G H I J
--출력 예
-A D G
+	 * 0804
+	 * 
+	 * 100 개의 정수를 저장할 수 있는 배열을 선언하고 정수를 차례로 입력받다가 0 이 입력되면 0 을 제외하고 그 때까지 입력된 정수를 가장
+	 * 나중에 입력된 정수부터 차례대로 출력하는 프로그램을 작성하시오.
+	 * 
+	 * -입력 예 3 5 10 55 0 -출력 예 55 10 5 3
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String[] a = new String[10];
-		
-		for(int i = 0; i < 10 ; i++) {
-			a[i] = sc.next();
+
+		int[] a = new int[100];
+
+		int i = 0;
+		while (true) {
+			a[i] = sc.nextInt();
+			if (a[i] == 0) {
+				for (int j = i - 1; j >= 0; j--) {
+					System.out.print(a[j] + " ");
+				}
+				break;
+			}
+			i++;
 		}
 		sc.close();
-		
-		System.out.print(a[0]+" "+a[3]+" "+a[6]);
 	}
 }
