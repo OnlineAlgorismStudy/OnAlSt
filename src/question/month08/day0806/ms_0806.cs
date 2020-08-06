434a79626a9bfb56c9d52d9ba22c0dad61189798
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-06 10:10:12 +0900

    민석스

---
+++ src/question/month08/day0806/ms_0806.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace algorithm_29
+{
+    class ms_0806
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+
+            for(int i = 0; i < data.Length; i++) {
+                int temp = Convert.ToInt32(Console.ReadLine());
+                
+                if(temp > 1000) {
+                    i--;
+                    continue;
+                }
+
+                data[i] = temp;
+            }
+
+            int min = 1000;
+
+            for(int i = 0; i < data.Length; i++) {
+                if(data[i] < min) {
+                    min = data[i];
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
