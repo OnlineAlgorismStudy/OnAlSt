a4793b6cca8ca0284ce6dced8b8b14c473fb82d2
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-08 20:28:25 +0900

    3다수

---
+++ src/question/month08/day0804/samdasu_0804.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace samdasu_0804
+{
+    class samdasu_0804
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            var n = Console.ReadLine();
+            var Nstring = n.Split(' ');
+
+            if(Nstring.Length <= 100)
+            {
+                for(int i = 0; i<Nstring.Length; i++)
+                {
+                    
+                    if (Nstring[i] == "0")
+                    {
+                        break;                        
+                    }
+                    arrayList.Add(Nstring[i]);
+
+                }
+                arrayList.Reverse();
+
+                for (int j = 0; j<arrayList.Count; j++)
+                {
+                    Console.Write(arrayList[j].ToString() + " ");
+                }
+            }
+
+        }
+    }
+}
