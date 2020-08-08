551cb73f44a150152a8d7c06fb73c284d2a540cb
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-08 20:27:35 +0900

    333다수

---
+++ src/question/month08/day0803/samdasu_0803.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0803
+{
+    class samdasu_0803
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+
+            var n = Console.ReadLine();
+            
+            var Nstring = n.Split(' ');
+            if (Nstring.Length >= 10)
+            {
+
+                for (int i = 0; i < Nstring.Length; i++)
+                {
+                    array.Add(Nstring[i]);
+                }
+
+                Console.Write(array[0] + " ");
+                Console.Write(array[3] + " ");
+                Console.Write(array[6] + " ");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("문자를 10개 이상 입력해 주세요");
+            }
+            
+        }
+    }
+}
