5f1e6c6ffe18ba28f3ff8f87bcb7cc2810abd16f
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-09 17:26:59 +0900

    Add files via upload

---
+++ src/question/month08/day0806/samdasu_0806.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samdasu_0806
+{
+    class samdasu_0806
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList n = new ArrayList();
+
+            var input = Console.ReadLine();
+            var _input = input.Split(' ');
+
+            if(_input.Length <= 10)
+            {
+                
+                Array.Sort(_input);
+                Console.WriteLine(_input[0]);
+            }
+            else
+            {
+                Console.Write("10개 이하로 정수 입력 바랍니다.");
+            }
+        }
+    }
+}
