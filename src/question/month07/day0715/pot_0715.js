6c65998660a003cdf8ada335bf330dab33aa0508
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-08-09 16:20:17 +0900

    Create pot_0715.js

---
+++ src/question/month07/day0715/pot_0715.js
@@ -0,0 +1,17 @@
+  data = prompt();
+   data1 =parseInt(data);
+   for(i=data1;i>0;i--){
+       for(j=0;i>=j;j++){
+        document.write("*");
+
+       }document.write("<br>");
+   }
+
+
+   for(i=0;i<data1;i++){
+       for(j=0;j<=i;j++){
+           document.write("*");
+       }document.write("<br>");
+   }
+   
+    </script>
