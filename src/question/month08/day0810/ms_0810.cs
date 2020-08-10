c33064f59f5651b2f34ea2630b0fc3f6bde20571
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-10 16:36:05 +0900

    민석스

---
+++ src/question/month08/day0810/ms_0810.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace algorithm_31
+{
+    class ms_0810
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            int hap = 0;
+            double avg = 0.0;
+
+            for(int i = 0; i < data.Length; i++) {
+                data[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i < data.Length; i++) {
+                if(i%2 != 0)
+                    hap += data[i];
+                else
+                    avg += data[i];
+            }
+
+            avg /= data.Length / 2;
+            Console.WriteLine($"sum : {hap}\navg : {avg:.0}");
+
+        }
+    }
+}
