9ddf459388d058768473035c67b3b184281946bf
금강누나 <52662748+itkk0306@users.noreply.github.com>
2020-08-12 00:17:44 +0900

    괴_0813

---
+++ src/question/month08/day0813/goe_0813.java
@@ -0,0 +1,37 @@
+package OAS;
+
+import java.util.Scanner;
+
+public class goe_0813 {
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+
+        int num, max = 0, min = 999;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < array.length; i++){
+            num = sc.nextInt();
+
+            if (num == 999){
+                break;
+            }
+
+            array[i] = num;
+
+            if(max < array[i]){
+                max = array[i];
+            }else if(min > array[i]){
+                min = array[i];
+            }
+
+        }
+
+        sc.close();
+
+        System.out.println("max: " + max);
+        System.out.println("min: " + min);
+
+    }
+}
