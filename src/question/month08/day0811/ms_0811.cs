9e2f0f5289d77fd7b2bb2206fb53310230575676
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-11 17:27:36 +0900

    민석스

---
+++ src/question/month08/day0811/ms_0811.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace algorithm_32
+{
+    class ms_0811
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+
+            for(int i = 0; i < data.Length; i++) {
+                data[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i < data.Length - 1; i++) {
+                int half = i;
+                
+                for(int j = i+1; j < data.Length; j++) {
+                    if(data[half] < data[j]) {
+                        half = j;
+                    }
+
+                    int temp = data[i];
+                    data[i] = data[half];
+                    data[half] = temp;
+                }
+            }
+
+            foreach(int value in data) {
+                Console.Write($"{value} ");
+            }
+        }
+    }
+}
