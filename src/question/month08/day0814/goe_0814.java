4c27dc04c1a31f285a855ded7c4e6f22fe22bd62
금강누나 <52662748+itkk0306@users.noreply.github.com>
2020-08-12 00:37:17 +0900

    괴_0814

---
+++ src/question/month08/day0814/goe_0814.java
@@ -0,0 +1,37 @@
+package OAS;
+
+import java.util.Scanner;
+
+public class goe_0814 {
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+
+        int num, cnt = 0, sum = 0;
+        double avg;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < array.length; i++){
+            num = sc.nextInt();
+
+            if(num == 0){
+                break;
+            }
+
+            array[i] = num;
+
+            if(array[i] % 5 == 0){
+                sum += array[i];
+                cnt++;
+            }
+        }
+        sc.close();
+
+        avg = (double)sum/cnt;
+
+        System.out.println("Multiples of 5: " + cnt);
+        System.out.println("sum: " + sum);
+        System.out.println("avg: " + Math.round(avg*100)/100.0);
+    }
+}
