6c0f4b9886f18ffd76ffd07ea07cc2537bcf4ca3
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-12 09:42:04 +0900

    민석스

---
+++ src/question/month08/day0812/ms_0812.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace algorithm_33
+{
+    class ms_0812
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            int odd = 0, even = 0;
+
+            for(int i = 0; i < data.Length; i++) 
+            {
+                data[i] = Convert.ToInt32(Console.ReadLine());
+
+                if(i%2 == 0)
+                    odd += data[i];
+                else
+                    even += data[i];
+            }
+
+            Console.WriteLine($"odd : {odd}\neven : {even}");
+        }
+    }
+}
