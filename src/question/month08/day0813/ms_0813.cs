3900176f56d8b338abc4d7468d69e8ec87a6429e
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-13 17:36:51 +0900

    민석스

---
+++ src/question/month08/day0813/ms_0813.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace algorithm_34
+{
+    class ms_0813
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[100];
+            int i = 0, count = 0;
+
+            while(i < data.Length) 
+            {
+                int digit = Convert.ToInt32(Console.ReadLine());
+
+                if(-1000 < digit && digit < 1000) 
+                {
+                    if(digit != 999) 
+                    {
+                        data[i] = digit;
+                        count++;
+                    } 
+                    else break;
+                } 
+                else continue;
+
+                i++;
+            }
+            
+            int max = -1000, min = 1000;
+
+            for(i = 0; i < count; i++) 
+            {
+                if(max < data[i]) max = data[i];
+                if(min > data[i]) min = data[i];
+            }
+
+            Console.WriteLine($"max : {max}\nmin : {min}");
+        }
+    }
+}
