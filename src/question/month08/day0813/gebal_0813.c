ee10b34cff4c26014abd80f02a1c9be28b09a7c7
kimjochi <67910216+kimjochi@users.noreply.github.com>
2020-08-13 10:07:10 +0900

    괴발자 0813

---
+++ src/question/month08/day0813/gebal_0813.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int n, max = 0, min = 1000;
+	while (1)
+	{
+		scanf("%d", &n);
+		if (n == 999) break;
+		if (n > max) max = n;
+		else if (n < min) min = n;
+	}
+
+	printf("max : %d\n", max);
+	printf("min: %d", min);
+}
