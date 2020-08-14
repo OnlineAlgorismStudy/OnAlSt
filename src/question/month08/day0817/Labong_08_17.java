729ed8cd0ec1b71e7be2cf4fc7e90472046a5468
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-08-15 01:49:22 +0900

    라봉
    라봉

---
+++ src/question/month08/day0817/Labong_08_17.java
@@ -0,0 +1,38 @@
+package august;
+
+import java.util.Scanner;
+
+public class Labong_08_17 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] num = new int[100];
+		int count = 0;
+		String result = "";
+		
+		for(int i=0; i<num.length; i++) {
+			num[i] = sc.nextInt();
+			
+			if(num[i] == 0) {break;}
+			
+			if(num[i] %2 == 0) {
+				num[i] = num[i]/2;
+				result += num[i]+" ";
+				count++;
+				
+			}else{
+				num[i] = num[i]*2;
+				result += num[i]+" ";
+				count++;
+			}
+		}
+		
+		
+		
+		System.out.println(count + "\n"+result);
+		// 8 10 5 15 100 0
+	}
+
+}
