bffdbf7c0a0d2beb5244828fc33768a9a937c78b
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-14 16:45:53 +0900

    민석스

---
+++ src/question/month08/day0814/ms_0814.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace algorithm_35
+{
+    class ms_0814
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[100];
+            int count = 0, i = 0;
+
+            while(i < data.Length) 
+            {
+                int temp = Convert.ToInt32(Console.ReadLine());
+
+                if(temp != 0) 
+                {
+                    data[i] = temp;
+                    count++;
+                }
+                else break;
+
+                i++;
+            }
+
+            int sum = 0, multiples = 0;
+
+            for(i = 0; i < count; i++) 
+            {
+                if(data[i]%5 == 0) 
+                {
+                    sum += data[i];
+                    multiples++;
+                }
+            }
+
+            double avg = sum / Convert.ToDouble(multiples);
+
+            Console.WriteLine($"Multiples of 5 : {multiples}");
+            Console.WriteLine($"sum : {sum}\navg : {avg:.0}");
+        }
+    }
+}
