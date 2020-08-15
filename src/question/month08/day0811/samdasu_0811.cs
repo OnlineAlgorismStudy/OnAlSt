2a287cff47bc6ad35d7d4ddea498bdc4f1b69839
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-15 20:47:59 +0900

    33 333 다수

---
+++ src/question/month08/day0811/samdasu_0811.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0811
+{
+    class samdasu_0811
+    {
+        static void Main(string[] args)
+        {
+
+            //var input = new int[10];
+            var input = (Console.ReadLine());
+            var _input = input.Split(' ');
+
+            ArrayList n = new ArrayList();
+
+            for(int i = 0; i<_input.Length; i++)
+            {
+                n.Add(_input[i]);
+            }
+            n.Sort();
+            n.Reverse();
+            
+
+            foreach (var value in n)
+            {
+                Console.Write("{0} ", value);
+            }
+        }
+    }
+}
