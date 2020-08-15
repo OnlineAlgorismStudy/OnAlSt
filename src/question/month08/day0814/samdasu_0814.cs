0bbb1e27dc2b910615a6af36d7a52855363ca7c7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-15 20:43:53 +0900

    3333다수

---
+++ src/question/month08/day0814/samdasu_0814.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0814
+{
+    class samdasu_0814
+    {
+        static void Main(string[] args)
+        {
+            var n = (Console.ReadLine());
+            var n_input = n.Split(' ');
+
+            double sum = 0.0;
+            ArrayList five = new ArrayList();
+            ArrayList allSum = new ArrayList();
+
+            for (int i = 0; i < n_input.Length; i++)
+            {
+                allSum.Add(n_input[i]);
+                if (Convert.ToInt32(allSum[i]) % 5 == 0 && Convert.ToInt32(allSum[i]) !='0')
+                {
+                    five.Add(allSum[i]);
+                                        
+                    sum += Convert.ToInt32(allSum[i]);
+
+                }
+                else { continue; }
+
+                if (n_input[i] == "0")
+                {
+                    five.RemoveAt(Convert.ToInt32(n_input[n_input.Length - 1]));
+                    Console.WriteLine("Multiples of 5 : {0}", five.Count);
+                    Console.WriteLine("sum : {0}", sum);
+                    Console.WriteLine("avg : {0:0.0}", Convert.ToDouble(sum / (five.Count)));
+                }
+                else
+                {
+                    continue;
+                }
+            }            
+        }
+    }
+}
