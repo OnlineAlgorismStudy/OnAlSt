17dc128b2807d0772c6baadd6239aeb5a64a6ccb
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-15 19:46:48 +0900

    33다수

---
+++ src/question/month08/day0812/samdasu_0812.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0812
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = (Console.ReadLine());
+            var n_input = n.Split(' ');
+
+            ArrayList one = new ArrayList();
+            ArrayList two = new ArrayList();
+
+            int add = 0;
+            double even = 0.0;
+
+
+
+
+            for (int i = 0; i < n_input.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    one.Add(n_input[i]);
+                }
+                else
+                {
+                    two.Add(n_input[i]);
+                }
+            }
+
+            for (int j = 0; j < one.Count; j++)
+            {
+                add += Convert.ToInt32(one[j]);
+
+
+            }
+
+            for (int k = 0; k < two.Count; k++)
+            {
+                even += Convert.ToInt32(two[k]);
+            }
+
+
+            Console.WriteLine("sum : {0}", add);
+            Console.WriteLine("even : {0}", even);
+        }
+    }
+}
