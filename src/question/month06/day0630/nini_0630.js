15e01b993c25d1fa224b22dc9b53500f1623e091
smmoon <36869648+seonminmoon@users.noreply.github.com>
2020-08-15 22:26:29 +0900

    ninis onalst

---
+++ src/question/month06/day0630/nini_0630.js
@@ -0,0 +1,12 @@
+let iNum = 0,
+    iNum2 = 0;
+
+iNum = 55;
+iNum2 = 10;
+
+console.log("55 - 10 = " + (iNum - iNum2));
+
+iNum = 2008;
+iNum2 = 1999;
+
+console.log("2008 - 1999 = " + (iNum - iNum2));
