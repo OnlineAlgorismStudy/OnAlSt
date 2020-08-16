d9af669a8f23c17b6b4bf8dd6d5fae9c68a7f9eb
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-08-17 00:17:49 +0900

    라봉
    라봉

---
+++ src/question/month08/day0820/Labong_08_20.java
@@ -0,0 +1,28 @@
+package august;
+
+import java.util.Scanner;
+
+public class Labong_08_20 {
+
+	public static void main(String[] args) {
+		// 5 8 10 6 4
+		// 11 20 1 13 2
+		// 7 9 14 22 3
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] num = {		{5, 8, 10, 6, 4},
+							{11, 20, 1, 13, 2},
+							{7, 9, 14, 22, 3}
+					  }; 
+		
+		for(int i=0; i<num.length;i++) {
+			for(int k=0; k<5;k++) {
+				System.out.printf("%2d   ",num[i][k]);
+			}
+			System.out.println();
+		}
+
+		
+	}
+
+}
