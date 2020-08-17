72b5263f9bee6211604a39dc10f97bc877860b40
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-17 17:30:34 +0900

    민석스

---
+++ src/question/month08/day0817/ms_0817.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace algorithm_36
+{
+    class ms_0817
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[100];
+            int count = 0, i = 0;
+
+            while(i < data.Length) 
+            {
+                int temp = Convert.ToInt32(Console.ReadLine());
+
+                if(temp != 0) 
+                {
+                    data[i] = temp;
+                    count++;
+                } else break;
+
+                i++;
+            }
+
+            Console.WriteLine(count);
+
+            for(i = 0; i < count; i++) 
+            {
+                if(data[i]%2 == 0)
+                    Console.Write(data[i]/2);
+                else
+                    Console.Write(data[i]*2);
+
+                Console.Write(' ');
+            }
+        }
+    }
+}
