3898df08c7a17c714f6928cace306329169cc362
kimjochi <67910216+kimjochi@users.noreply.github.com>
2020-08-18 09:40:17 +0900

    Update gebal_0818.c

--- src/question/month08/day0818/gebal_0818.c
+++ src/question/month08/day0818/gebal_0818.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 int compare(const void* first, const void* second) {
 	if (*(int*)first < *(int*)second) return 1;
 	else if (*(int*)first > *(int*)second) return -1;
