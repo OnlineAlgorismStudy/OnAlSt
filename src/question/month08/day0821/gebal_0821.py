0a74fb4bd5b71455b99436600d8f09337e0289e6
kimjochi <67910216+kimjochi@users.noreply.github.com>
2020-08-18 15:35:36 +0900

    Update gebal_0821.py

--- src/question/month08/day0821/gebal_0821.py
+++ src/question/month08/day0821/gebal_0821.py
@@ -1,4 +1,10 @@
 import numpy as np
-first = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-second = np.array([[1, 4, 7, 8], [3, 6, 9, 8]])
+first = []
+second = []
+print("first array")
+[first.append(input().split(' ')) for x in range(2)]
+print("second array")
+[second.append(input().split(' ')) for i in range(2)]
+first = np.array(first, np.int32)
+second = np.array(second, np.int32)
 [[print(v, end=' ') if i != 3 else print(v) for i, v in enumerate(x)] for x in first * second]
