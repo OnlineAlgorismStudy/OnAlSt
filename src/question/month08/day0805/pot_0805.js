34483ffc5dde50b520cbfc02ef2ec40dd9e061cf
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-08-18 23:20:04 +0900

    Create pot_0805.js

---
+++ src/question/month08/day0805/pot_0805.js
@@ -0,0 +1,12 @@
+    <script>
+        var array1 = [85.6,79.5,83.1,80.0,78.2,75.0]
+        num1 = prompt();
+        num2 = prompt();
+
+        result = array1[num1-1] + array1[num2-1];
+        document.write(result.toFixed(1));
+        
+        
+
+   
+    </script>
