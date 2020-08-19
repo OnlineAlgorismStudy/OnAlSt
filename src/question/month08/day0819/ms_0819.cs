ad904f3e7d898d5c61bbfea3ec15abc56e84b2ae
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-19 20:43:25 +0900

    민석스

---
+++ src/question/month08/day0819/ms_0819.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace algorithm_38
+{
+    class ms_0819
+    {
+        static void Main(string[] args)
+        {
+            int[] charCount = new int[26];
+            char cnd;
+
+            while(true) {
+                cnd = Convert.ToChar(Console.ReadLine());
+
+                if(!('A' <= cnd && cnd <= 'Z'))
+                    break;
+
+                charCount[cnd - 'A']++;
+            }
+
+            for(int i = 0; i < charCount.Length; i++)
+                if(charCount[i] != 0)
+                    Console.WriteLine($"{Convert.ToChar('A' + i)} : {charCount[i]}");
+        }
+    }
+}
