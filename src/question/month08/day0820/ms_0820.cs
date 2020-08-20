721bad9d57001e240c57367bf0934899e545c98b
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-20 18:16:54 +0900

    민석스

---
+++ src/question/month08/day0820/ms_0820.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace algorithm_39
+{
+    class ms_0820
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[3, 5] { { 5, 8, 10, 6, 4 },
+                                          { 11, 20, 1, 13, 2 },
+                                          { 7, 9, 14, 22, 3 } };
+
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 5; j++) {
+                    Console.Write($"{data[i, j], 2}   ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
