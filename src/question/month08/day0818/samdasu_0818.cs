7406ec722a61c28b86337e1aa65bfae1dd9d239a
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-21 20:21:22 +0900

    3다수ㅠ

---
+++ src/question/month08/day0818/samdasu_0818.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0818
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var input_score = Console.ReadLine();
+            var n_score = input_score.Split(' ');
+
+            ArrayList ar = new ArrayList();
+                                    
+            for(int i = 0; i<n_score.Length; i++)
+            {
+                ar.Add(n_score[i]);
+            }
+
+            ar.RemoveAt(0);
+            ar.Sort();
+            ar.Reverse();
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                Console.WriteLine(ar[i]);
+            }
+        }
+    }
+}
