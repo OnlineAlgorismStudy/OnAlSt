1c03ed49a5b7d44b21b8e516516e0c9d3b79db7d
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-08-21 14:11:19 +0900

    바름

---
+++ src/question/month06/day0629/br_day29.js
@@ -0,0 +1,9 @@
+let kor = 90;
+let mat = 80;
+let eng = 100;
+
+console.log("kor " + kor);
+console.log("mat " + mat);
+console.log("eng " + eng);
+console.log("sum " + (kor+mat+eng));
+console.log("avg " + ((kor+mat+eng)/3));
