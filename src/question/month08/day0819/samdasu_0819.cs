c7508ce1f5c645c09d667531aca5cd0a186f675e
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-23 15:06:07 +0900

    3다수

---
+++ src/question/month08/day0819/samdasu_0819.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace samdasu_0819
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[26];
+            char ch;
+
+            while (true)
+            {
+                ch = Convert.ToChar(Console.ReadLine());
+
+                if(!(ch>=65 && ch <= 90))
+                {
+                    break;
+                }
+                else {
+                    n[ch - 65]++;
+                }
+
+                
+            }
+
+            for(int i = 0; i<n.Length; i++)
+            {
+                if(n[i] != 0)
+                {
+                    Console.WriteLine("{0} : {1}", Convert.ToChar('A' + i), n[i]);
+                }
+            }
+        }
+    }
+}
