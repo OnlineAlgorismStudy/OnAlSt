bdda85050d1aa7f62238c721372382d44ae5a465
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-23 15:22:24 +0900

    3다슈

---
+++ src/question/month08/day0820/samdasu_0820.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace samdasu_0820
+{
+    class samdasu_0820
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { {5,8,10,6,4 },{ 11,20,1,13,2},{7,9,14,22,3 } };
+
+            for(int i = 0; i<3; i++)
+            {
+                for(int j = 0; j<5; j++)
+                {
+                    Console.Write("  {0}", arr[i,j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
