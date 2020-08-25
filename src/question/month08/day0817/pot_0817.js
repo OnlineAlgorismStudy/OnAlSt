4f69bdacfa314b43f345c6bafc0cffdcebb57b83
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-08-25 13:10:47 +0900

    Update pot_0817.js

--- src/question/month08/day0817/pot_0817.js
+++ src/question/month08/day0817/pot_0817.js
@@ -1,28 +1,28 @@
 <script>
-        //입력한 수 count
-        //count 홀수 입력한수 * 2
-        //count 짝수 입력한수 / 2
-        var arr = new Array(100);
-        var count = 0;
-        //입력,count 증가
-        for(i=0;i<arr.length;i++){
-            arr[i] = parseInt(prompt());
-            count++;
-            //0 입력 > break;
-            if(arr[i] == 0){
-                break;
-            }
+    //입력한 수 count
+    //count 홀수 입력한수 * 2
+    //count 짝수 입력한수 / 2
+    var arr = new Array(100);
+    var count = 0;
+    //입력,count 증가
+    for(i=0;i<arr.length;i++){
+        arr[i] = parseInt(prompt());
+        count++;
+        //0 입력 > break;
+        if(arr[i] == 0){
+            break;
         }
-        //입력 개수 출력
-        document.write("입력 개수 : " + (count-1)+"<br>");
-        
-        //입력한 개수 만큼 반복
-        for(i=0;i<count-1;i++){
-            //입력한 개수가 짝수라면
-            if((count-1) % 2 ==0){
-                document.write(arr[i] / 2 +"\t");
-            }else{//홀수라면
-                document.write(arr[i] * 2 +"\t");
-            }
+    }
+    //입력 개수 출력
+    document.write("입력 개수 : " + (count-1)+"<br>");
+    
+    //입력한 개수 만큼 반복
+    for(i=0;i<count-1;i++){
+        //입력한 값이 짝수라면
+        if((arr[i]) % 2 ==0){
+            document.write(arr[i] / 2 +"\t");
+        }else{//홀수라면
+            document.write(arr[i] * 2 +"\t");
         }
-    </script>
+    }
+</script>
