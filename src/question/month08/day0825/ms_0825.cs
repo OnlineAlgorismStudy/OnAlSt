d0e58f426589d8ac1f7d70d86924e236f5838eac
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-25 22:11:01 +0900

    민석스

---
+++ src/question/month08/day0825/ms_0825.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace algorithm_42
+{
+    class ms_0825
+    {
+        static void Main(string[] args)
+        {
+            int[] students = new int[100];
+            int[] nss = new int[10]; // number of students by score
+            int i = 0;
+
+            while(true) 
+            {
+                int temp = Convert.ToInt32(Console.ReadLine());
+
+                if(temp != 0) 
+                    nss[(temp/10)-1]++;
+                else break;
+            }
+
+            for(i = nss.Length-1; i >= 0; i--) 
+                if(nss[i] != 0)
+                    Console.WriteLine($"{(i+1)*10} : {nss[i]} person");
+        }
+    }
+}
