9461865950ad270eda61ee053d48d8f812ee497d
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-08-25 22:40:21 +0900

    Update Jjingu_0825.py

--- src/question/month08/day0825/Jjingu_0825.py
+++ src/question/month08/day0825/Jjingu_0825.py
@@ -4,15 +4,17 @@ import collections
 import numpy as np
 
 ret = True
+count = 0
 num_list = []
 while ret:
     a = int(input())
-    if a == 0:
+    if a == 0 or count ==100:
         ret = False
         break
     else:
         num_list.append(a)
-
+        count +=1
+        
 num_arr = np.array(num_list)/10
 num_arr = np.floor(num_arr)*10
 num_arr = np.sort(num_arr)[::-1]
