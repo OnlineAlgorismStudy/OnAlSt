49f73f54a1bf5f5eb75da0208bc027a4762da8cc
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-08-26 22:48:48 +0900

    라봉
    라봉

---
+++ src/question/month08/day0827/Labong_08_27.java
@@ -0,0 +1,24 @@
+package august;
+
+public class Labong_08_27 {
+
+	public static void main(String[] args) {
+		
+		int result = 0;
+		int[][] num = {	{3, 5, 9},
+						{2, 11 ,5},
+						{8, 30, 10},
+						{22, 5, 1}
+					  };
+		
+		for(int i=0; i<4; i++) {
+			for(int i2=0; i2<3; i2++) {
+				System.out.print(num[i][i2] + " ");
+				result += num[i][i2];
+			}
+			System.out.println();
+		} //for
+			System.out.println(result);
+	} // main
+
+}
