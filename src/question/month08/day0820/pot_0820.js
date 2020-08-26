835cc130fe99d9e81ff7fe7714b3f77773bff985
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-08-27 00:08:52 +0900

    Update pot_0820.js

--- src/question/month08/day0820/pot_0820.js
+++ src/question/month08/day0820/pot_0820.js
@@ -1,10 +1,14 @@
-<script>
+    <script>
         //3행 5열 값 출력 , 2차원배열
         var arr1 =[[5,8,10,6,4],[11,20,1,13,2],[7,9,14,22,3]]
         for(i=0;i<3;i++){
             for(j=0;j<5;j++){
-                document.writeln(arr1[i][j]);
+                if(arr1[i][j]<10){
+                    document.write('&nbsp&nbsp&nbsp '+arr1[i][j]);
+                }else{
+                    document.write(' &nbsp&nbsp'+arr1[i][j]);
+                }
             }
-            document.writeln("<br>");
+            document.write("<br>");
         }
     </script>
