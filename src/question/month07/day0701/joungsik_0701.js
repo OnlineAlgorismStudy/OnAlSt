76f4ed2ced0311841b691d83f416f0273bade5ab
JoungSik <tjstlr2010@gmail.com>
2020-08-26 12:26:35 +0900

    Update joungsik_0701.js

--- src/question/month07/day0701/joungsik_0701.js
+++ src/question/month07/day0701/joungsik_0701.js
@@ -1,5 +1,18 @@
 const [yard, inch] = [91.44, 2.54];
 const [num1, num2] = [2.1, 10.5];
 
-console.log(`${num1}yd = ${(num1 * yard).toFixed(1)}cm`);
-console.log(`${num2}in = ${(num2 * inch).toFixed(1)}cm`);
+const result = (num1, num2) =>
+  num1.toFixed(0).toString().length > num2.toFixed(0).toString().length
+    ? Math.pow(10, num1.toFixed(0).toString().length - 1)
+    : Math.pow(10, num2.toFixed(0).toString().length - 1);
+
+console.log(
+  `${num1 < result(num1, num2) ? " " + num1 : num1}yd = ${
+    num1 * yard < result(num1 * yard, num2 * inch) ? " " + (num1 * yard).toFixed(1) : (num1 * yard).toFixed(1)
+  }cm`
+);
+console.log(
+  `${num2 < result(num1, num2) ? " " + num2 : num2}in = ${
+    num2 * inch < result(num1 * yard, num2 * inch) ? " " + (num2 * inch).toFixed(1) : (num2 * inch).toFixed(1)
+  }cm`
+);
