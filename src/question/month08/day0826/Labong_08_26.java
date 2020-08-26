773819f2d3421d84c41d86161c4018d151517971
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-08-26 13:03:09 +0900

    라봉
    라봉

---
+++ src/question/month08/day0826/Labong_08_26.java
@@ -0,0 +1,32 @@
+package august;
+
+import java.util.Scanner;
+
+public class Labong_08_26 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] ban = new int[4][1];
+		int count =0;
+		
+		for(int i=0; i<4; i++) {
+			System.out.println(i+1 + "class? ");
+			for(int i2=0; i2<3; i2++) {
+				//ban[i][i2] = sc.nextInt();
+				count += sc.nextInt();
+			}
+			ban[i][0] = count;
+			count = 0;
+		} // for
+
+		sc.close();
+		
+		for(int k=0; k<ban.length; k++) {
+			System.out.println(k+1 + "class : "+ ban[k][0]);
+		}// for
+		
+	} // main
+
+}
