55e5e514cb38881ee275d1f8c73351bd48a0a7e7
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-29 16:07:20 +0900

    늦은 민석스

---
+++ year2020/month08/day0827/ms_0827.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace algorithm_44
+{
+    class ms_0827
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[4, 3]
+            {
+                { 3, 5, 9 },
+                { 2, 11, 5 },
+                { 8, 30, 10 },
+                { 22, 5, 1 }
+            };
+            int sum = 0;
+
+            for(int i = 0; i < 4; i++) {
+                for(int j = 0; j < 3; j++) {
+                    Console.Write(data[i, j]);
+                    sum += data[i, j];
+
+                    if(j != 3)
+                        Console.Write(' ');
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
