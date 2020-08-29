2b348e6c95611f43ae5d91c6f868303dc1afb964
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-29 16:03:44 +0900

    늦은 민석스

---
+++ year2020/month08/day0826/ms_0826.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace algorithm_43
+{
+    class ms_0826
+    {
+        static void Main(string[] args)
+        {
+            int[] score = new int[4] { 0, 0, 0, 0 };
+
+            for(int i = 0; i < 4; i++) {
+                Console.Write($"{i + 1}class? ");
+
+                string[] data = Console.ReadLine().Split(' ');
+
+                foreach(var value in data) {
+                    score[i] += Convert.ToInt32(value);
+                }
+            }
+
+            for(int i = 0; i < score.Length; i++) {
+                Console.WriteLine($"{i + 1}class : {score[i]}");
+            }
+        }
+    }
+}
