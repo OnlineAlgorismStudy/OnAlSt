2c2af7c58df5f3d979aa0c375620b84a0f85026a
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-08-30 02:47:22 +0900

    라봉
    라봉

---
+++ year2020/month09/day0904/Labong_09_04.java
@@ -0,0 +1,33 @@
+package september;
+
+import java.util.Scanner;
+
+public class Labong_09_04 {
+
+	static int reint(int[] num) {
+		int result = 0;
+		
+		for(int k=0; k<3; k++) {
+			for(int k2=0; k2<3; k2++) {
+				if(result == 0) {
+					result = num[k2];
+				}else if(result < num[k2]) {
+					result = num[k2];
+				}
+			} // for
+		} // for 	
+		return result;
+	}
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			num[i] = sc.nextInt();
+		}
+		System.out.println(reint(num));	
+	} // main
+
+}
