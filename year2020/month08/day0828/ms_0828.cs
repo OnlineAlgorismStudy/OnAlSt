715a6a2bad81aff95890dcd38fda274a72527f22
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-29 16:19:56 +0900

    늦은 민석스

---
+++ year2020/month08/day0828/ms_0828.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace algorithm_45
+{
+    class ms_0828
+    {
+        static void Main(string[] args)
+        {
+            int[] row = new int[4] { 0, 0, 0, 0 };
+            int[] column = new int[2] { 0, 0 };
+            int total = 0;
+
+            for(int i = 0; i < 4; i++) 
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int j = 0;
+
+                foreach(var value in input) {
+                    row[i] += Convert.ToInt32(value);
+                    column[j] += Convert.ToInt32(value);
+                    total += Convert.ToInt32(value);
+                    j++;
+                }
+            }
+
+            Console.WriteLine($"{row[0]/2} {row[1]/2} {row[2]/2} {row[3]/2}");
+            Console.WriteLine($"{column[0]/4} {column[1]/4}");
+            Console.WriteLine(total/8);
+        }
+    }
+}
