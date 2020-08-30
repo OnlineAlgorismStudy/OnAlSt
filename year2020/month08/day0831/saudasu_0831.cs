d2cb4e5661cba5c3266609547d7719c2736491fb
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-30 18:36:58 +0900

    삼다수

---
+++ year2020/month08/day0831/saudasu_0831.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu_0831
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] n = new char[3, 5];
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0;j<5; j++)
+                {
+                    n[i, j] = Convert.ToChar(Console.ReadLine());
+                }
+            }
+            for(int i = 0; i<3; i++)
+            {
+                for (int j = 0; j < 5; j++) 
+                {
+                    Console.Write("{0} ", Convert.ToChar(n[i, j] + 32));
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
