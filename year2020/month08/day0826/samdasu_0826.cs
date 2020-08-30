06cbcf23ee11a909c9bd0fbb21228143fc8da643
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-30 18:15:04 +0900

    삼다수 0826

---
+++ year2020/month08/day0826/samdasu_0826.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace samdasu_0826
+{
+    class samdasu_0826
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[4, 3];
+            int[] sum = new int[4];
+            int num = 0;
+
+            for (int i = 0; i < 4; i++) 
+            {
+                Console.WriteLine("{0}class? ", i + 1);
+
+                for(int j=0; j < 3; j++)
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                    arr[i,j] = num;
+                }
+                //Console.WriteLine(" ");
+            }
+
+            for(int i = 0; i<4; i++)
+            {
+                for(int j = 0; j<3; j++)
+                {
+                    sum[i] += arr[i, j];
+                }
+                Console.WriteLine("{0}class: {1}", i + 1, sum[i]);
+            }
+        }
+    }
+}
