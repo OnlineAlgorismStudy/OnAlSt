d8fa83123ab3fac749a083f337f4e7fc250868e5
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-08-30 18:14:36 +0900

    삼다수0825

---
+++ year2020/month08/day0825/samdasu_0825.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace samdasu_0825
+{
+    class samdasu_0825
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+
+            int[] num = new int[100];
+            int sum = 0;
+
+            for(int i = 0; i<num.Length; i++)
+            {
+                sum = Convert.ToInt32(Console.ReadLine());
+
+                if(sum == 0) { break; }
+
+                for (int j = 0; j <= 10; j++)
+                {
+                    if (j == sum / 10)
+                    {
+                        num[j]++;
+                    }
+                }
+            }
+
+            
+
+            for (int i = 10; i >= 0; i--)
+            {
+                if (num[i] >= 0)
+                {
+                    if (num[i] == 0) { continue; }
+                }
+                if (i == 0)
+                {
+                    Console.WriteLine("{0} : {1} person", i + 1, num[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0} : {1} person", i * 10, num[i]);
+                }
+            }
+        }
+    }
+}
