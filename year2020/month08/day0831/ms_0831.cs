8b4afaac7abe6d661def94166b9a489ed9539575
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-08-31 17:12:53 +0900

    민석스

---
+++ year2020/month08/day0831/ms_0831.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace algorithm_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] data = new char[3, 5];
+
+            for(int i = 0; i < 3; i++) 
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int j = 0;
+
+                foreach(var word in inputs) 
+                {
+                    char temp = Convert.ToChar(word);
+
+                    if('A' <= temp && temp <= 'Z') 
+                    {
+                        data[i, j] = temp;
+                        j++;
+                    } 
+                    else 
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+            }
+
+            for(int i = 0; i < 3; i++) 
+            {
+                for(int j = 0; j < 5; j++) 
+                {
+                    Console.Write($"{Convert.ToChar(data[i, j] + 32)}");
+
+                    if(j != 5)
+                        Console.Write(' ');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
