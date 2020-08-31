2b201f2eb29a745b75e7021d7fa21bb1bbb8064d
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-08-31 15:27:01 +0900

    바름

---
+++ year2020/month09/day0910/BR_0910.java
@@ -0,0 +1,15 @@
+public class BR_0910 {
+
+	public static void main(String[] args) {
+		System.out.println("first");
+		printAt();
+		System.out.println("second");
+		printAt();
+		System.out.println("third");
+		printAt();
+	}
+
+	private static void printAt() {
+		System.out.println("@@@@@@@@@@");
+	}
+}
