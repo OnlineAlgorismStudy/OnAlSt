b541c1206ca43eb4ab2b47df34e89485ee49b126
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-08-31 15:22:45 +0900

    바름

---
+++ year2020/month09/day0909/BR_0909.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BR_0909 {
+
+	public static void main(String[] args) {
+		List<Integer> listDan = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in);
+		
+		int temp1 = sc.nextInt();
+		int temp2 = sc.nextInt();
+		if (temp1 == temp2) listDan.add(temp1);
+		else {
+			listDan.add(Math.min(temp1, temp2));
+			listDan.add(Math.max(temp1, temp2));
+		}
+		
+		sc.close();
+		
+		while (listDan.size() < Math.abs(temp1 - temp2) + 1) listDan.add(1, listDan.get(1) - 1);
+		for (int dan : listDan) System.out.println(printGugudan(dan));
+	}
+
+	private static String printGugudan (int dan) {
+		String gugudan = "== " + dan + "dan ==\n";
+		
+		for (int i = 1; i <= 9; i++) {
+			gugudan += dan + " * " + i + " = " + String.format("%2s", dan*i) + "\n";
+		}
+		
+		return gugudan;
+	}
+}
