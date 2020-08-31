dd8a2f8e2fa461b87a5a7238a054986397031a2f
sjl5411 <65157067+sjl5411@users.noreply.github.com>
2020-08-31 17:09:27 +0900

    바름

---
+++ year2020/month09/day0917/BR_0917.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BR_0917 {
+
+	private static List<List<Integer>> scores = new ArrayList<List<Integer>>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		while (size < 1 || size > 10) size = sc.nextInt();
+		int[] ary = new int[size];
+		
+		for (int i = 0; i < size; i++) {
+			ary[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		String strAry = "";
+		for (int i : sortDesc(ary)) {
+			strAry += i + " ";
+		}
+		System.out.println(strAry.trim());
+	}
+	
+	private static int[] sortDesc(int[] ary) {
+		for (int i = 0; i < ary.length-1; i++) {
+			for (int j = i+1; j < ary.length; j++) {
+				if (ary[i] < ary[j]) {
+					int tmp = ary[i];
+					ary[i] = ary[j];
+					ary[j] = tmp;
+				}
+			}
+		}
+		return ary;
+	}
+
+}
