fe01dbd3bd65d02a8a9bb9204f7b600b7f6f1707
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-09-01 08:37:58 +0900

    2020.09.01 : upload algo by Y

---
+++ year2020/month09/day0901/Y_0901.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+	
+	string s = "~!@#$^&*()_+|";
+	int num;
+	scanf("%d", &num);
+
+	for(int i=0; i<num; i++)
+	{
+		cout<<s<<endl;
+	}
+}
+
