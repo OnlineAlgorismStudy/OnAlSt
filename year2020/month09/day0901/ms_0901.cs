369e8d9b975b13ff258b2aef8933b4bc175322b3
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-01 23:22:27 +0900

    민석스

---
+++ year2020/month09/day0901/ms_0901.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace algorithm_47
+{
+    class ms_0901
+    {
+        // ~!@#$^&*()_+|
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < n; i++) {
+                printString();
+            }
+        }
+
+        static void printString() {
+            Console.WriteLine("~!@#$^&* ()_+|");
+        }
+    }
+}
