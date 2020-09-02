8aa4924dfade5bcf20d463fda17675877eb033f7
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-02 21:02:17 +0900

    민석스

---
+++ year2020/month09/day0902/ms_0902.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace algorithm_48
+{
+    class ms_0902
+    {
+        static void Area(int n) 
+        {
+            Console.WriteLine($"{(n * n * 3.14):.00}");
+        }
+
+        static void Main(string[] args)
+        {
+            Area(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
