028159238539e93cf0b76af716b83842220bec2d
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-09-02 21:13:20 +0900

    Update Jjingu_0901.py

--- year2020/month09/day0901/Jjingu_0901.py
+++ year2020/month09/day0901/Jjingu_0901.py
@@ -1,7 +1,8 @@
 #아대숲 찐구 0901 문제풀이
 
-a = int(input())
+def function_1(a):
+    for i in range(0,a):
+        print("~!@#$^&*()+|")
 
-for i in range(0,a):
-    print("~!@#$^&*()+|")
-   
+a = int(input())
+function_1(a)
