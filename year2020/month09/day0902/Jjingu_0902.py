5b5d0ef8685df51c5c9410ffa3a815399895d807
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-09-02 21:15:45 +0900

    Update Jjingu_0902.py

--- year2020/month09/day0902/Jjingu_0902.py
+++ year2020/month09/day0902/Jjingu_0902.py
@@ -1,5 +1,8 @@
 #아대숲 찐구 0902 문제풀이
 
-radius = float(input())
-area = 3.14*radius**2
-print("%0.2f" %area)1
+def area_circle(r):
+    area = 3.14*r**2
+    print("%0.2f" %area)
+
+r = int(input())
+area_circle(r)
