fbf237e64ad034d4892c72ccee09996dcfb70e91
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-03 15:19:51 +0900

    민석스

---
+++ year2020/month09/day0903/ms_0903.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace algorithm_49
+{
+    class Program
+    {
+        static void DrawRec(int n) 
+        {
+            int count = 1;
+
+            for(int i = 0; i < n; i++) 
+            {
+                for(int j = 0; j < n; j++) 
+                {
+                    Console.Write(count);
+                    count++;
+
+                    if(j != n)
+                        Console.Write(' ');
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if(0 < n && n < 100) 
+            {
+                DrawRec(n);
+            }
+        }
+    }
+}
