1ca6cd4556bf47c7efc6bab56985e4546f29b029
inji728 <67571622+inji728@users.noreply.github.com>
2020-09-04 08:19:55 +0900

    Add files via upload

---
+++ year2020/month09/day0904/모르는개산책_0904.py
@@ -0,0 +1,17 @@
+# 2020 09 04 Algorithm Study Day 51
+# Inji Seo
+
+'''
+세 개의 정수를 전달받아 최대값을 구하여 리턴하는 함수를 작성하고
+세 정수를 입력받아 최대값을 출력하는 프로그램을 작성하시오.
+
+입력 예: -10 115 33
+출력 예: 115
+'''
+
+mylist = input('').split()
+
+for i in range(len(mylist)):
+    mylist[i] = int(mylist[i])
+
+print(max(mylist[0], mylist[1], mylist[2]))
