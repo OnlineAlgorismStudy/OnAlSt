1fced271faca547996182b8c937b206011d86fab
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-06 18:47:54 +0900

    33다수

---
+++ year2020/month09/day0902/samdasu_0902.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace samdasu_0902
+{
+    class samdasu_0902
+    {
+        static void Main(string[] args)
+        {
+            int radius_input = Convert.ToInt32(Console.ReadLine());
+            double pi = Math.PI;
+
+
+            fn_Circlearea();
+            void fn_Circlearea()
+            {
+                Console.WriteLine("{0:.00}", pi * radius_input*radius_input);
+            }
+        }
+    }
+}
