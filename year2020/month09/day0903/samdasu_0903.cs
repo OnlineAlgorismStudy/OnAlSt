4419c9c37e2e50238bb010178cca79faa4e1e033
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-06 18:48:45 +0900

    333다수

---
+++ year2020/month09/day0903/samdasu_0903.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace samdasu_0903
+{
+    class samdasu_0903
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 100)
+            {
+                fn_rectangle();
+            }
+
+            void fn_rectangle()
+            {
+                for(int i = 1; i<=n*n; i++)
+                {
+                    Console.Write(i + " ");
+                    if ((i%Convert.ToDouble(n)) == 0)
+                    {
+                        Console.WriteLine(" ");
+                    }                   
+                }
+            }
+        }
+    }
+}
