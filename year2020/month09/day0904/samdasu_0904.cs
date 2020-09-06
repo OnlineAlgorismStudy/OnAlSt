564fce642107ece65071b07fe86d2f9d17237149
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-06 18:49:14 +0900

    33다슈슈

---
+++ year2020/month09/day0904/samdasu_0904.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0904
+{
+    class samdasu_0904
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[3];
+
+
+            for (int i =0; i < 3; i++)
+            {
+                int input = Int32.Parse(Console.ReadLine().ToString());
+                
+
+                n[i] = input;
+            }
+            
+            fn_maxNum(n);
+
+            int fn_maxNum(int[] n)
+            {
+                int result = 0;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (result < n[i])
+                    {
+                        result = n[i];
+                    }
+                }
+                return result;
+            }
+
+
+            Console.WriteLine(fn_maxNum(n));
+        }
+
+        
+    }
+}
