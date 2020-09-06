66079d70d5ce09587eca5c291a7ea6c6a24f2ea6
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-06 18:47:25 +0900

    333

---
+++ year2020/month09/day0901/samdasu_0901.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace samdasu_0901
+{
+    class samdasu_0901
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            for(int i =0;i<input;i++)
+            {
+                fn_printStr();
+            }
+            
+
+            void fn_printStr()
+            {
+                Console.WriteLine("~!@#$^*()_+|");
+            }
+        }
+        
+    }
+}
