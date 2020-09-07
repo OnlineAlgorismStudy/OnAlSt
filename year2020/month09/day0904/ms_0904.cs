0ac67ca326e2c634ec3002289bbe67f3bbb88c31
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-07 14:37:52 +0900

    민석스

---
+++ year2020/month09/day0904/ms_0904.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace algorithm_50
+{
+    class ms_0904
+    {
+        static int getMax(int[] data) 
+        {
+            int max = data[0];
+
+            for(int i = 1; i < data.Length; i++)
+                if(max < data[i])
+                    max = data[i];
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] data = new int[3];
+            
+            for(int i = 0; i < data.Length; i++)
+                data[i] = Convert.ToInt32(input[i]);
+
+            Console.Write(getMax(data));
+        }
+    }
+}
