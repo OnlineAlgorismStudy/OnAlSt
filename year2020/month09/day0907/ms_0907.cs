ea4af3d20f016c1666cd4f67d7a3823ac546ad4f
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-07 14:37:31 +0900

    민석스

---
+++ year2020/month09/day0907/ms_0907.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace algorithm_51
+{
+    class ms_0907
+    {
+        static int Power(int n, int count) 
+        {
+            if(count == 0)
+                return 1;
+            else
+                return n * Power(n, count-1);
+        }
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int num = Convert.ToInt32(input[0]);
+            int count = Convert.ToInt32(input[1]);
+            
+            Console.Write(Power(num, count));
+        }
+    }
+}
