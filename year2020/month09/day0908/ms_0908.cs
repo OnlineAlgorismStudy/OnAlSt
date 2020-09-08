4b7aeb36ec0b0707d9f66c1c1a511c0ccde57474
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-08 22:05:39 +0900

    민석스

---
+++ year2020/month09/day0908/ms_0908.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace algorithm_52
+{
+    class ms_0908
+    {
+        static void changeValue(ref int mult, ref int div) 
+        {
+                mult *= 2;
+                div /= 2;
+        }
+
+        static void Main(string[] args)
+        {
+            // x 가 작은 수, y 가 큰 수를 저장
+            string[] input = Console.ReadLine().Split(' ');
+            int x = Convert.ToInt32(input[0]);
+            int y = Convert.ToInt32(input[1]);
+
+            if(x > y) 
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+
+            changeValue(ref x, ref y);
+            Console.WriteLine($"{x} {y}");
+        }
+    }
+}
