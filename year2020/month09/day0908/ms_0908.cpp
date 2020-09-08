4cf0eeb5cede8acc5c61c773fde472c58f18d186
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-08 22:10:41 +0900

    민석스

---
+++ year2020/month09/day0908/ms_0908.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void changeValue(int* pointer);
+
+int main() {
+	// data[0] 이 작은 수, data[1] 이 큰 수를 저장
+	int data[2];
+
+	for (int i = 0; i < 2; i++)
+		scanf_s("%d", &data[i]);
+
+	if (data[0] > data[1]) {
+		int temp = data[0];
+		data[0] = data[1];
+		data[1] = temp;
+	}
+
+	changeValue(data);
+	cout << data[0] << ' ' << data[1] << endl;
+
+	return 0;
+}
+
+void changeValue(int* pointer) {
+	*pointer *= 2;
+	*(++pointer) /= 2;
+}
