fa86d04a5f3c1893870ab123725adbbb28c26317
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-09 14:06:09 +0900

    민석스

---
+++ year2020/month09/day0909/ms_0909.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace algorithm_53
+{
+    class ms_0909
+    {
+        static void MultTable(int start, int end) 
+        {
+            for(int i = start; i <= end; i++) 
+            {
+                Console.WriteLine($"== {i}dan ==");
+        
+                for(int j = 1; j <= 9; j++)
+                    Console.WriteLine($"{i} * {j} = {i*j, 2}");
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int start = Convert.ToInt32(input[0]);
+            int end = Convert.ToInt32(input[1]);
+
+            if(start > end) 
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            MultTable(start, end);
+        }
+    }
+}
