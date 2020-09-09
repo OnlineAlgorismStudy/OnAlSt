aa5f04cccd6c2aac4c9eb6c9a83a9485fb95e26d
JoungSik <tjstlr2010@gmail.com>
2020-09-09 12:52:03 +0900

    [Add] 0909

---
+++ year2020/month09/day0909/joungsik_0909.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const range = (start, end) => {
+  let arr = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const print = (min, max) => {
+  range(min, max).map((num) => {
+    console.log(`== ${num}dan ==`);
+    range(1, 9).map((word) => {
+      console.log(`${num} * ${word} = ${num * word > 9 ? num * word : " " + num * word}`);
+    });
+  });
+};
+
+rl.on("line", function (line) {
+  const nums = line.split(" ").map((num) => parseInt(num));
+  print(Math.min(...nums), Math.max(...nums));
+  rl.close();
+});
