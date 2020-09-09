9e13108804ca5cc82a590c1b92f1f275709d63ca
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-09-09 13:45:06 +0900

    라봉
    라봉

---
+++ year2020/month09/day0910/Labong_09_10.java
@@ -0,0 +1,26 @@
+package september;
+
+public class Labong_09_10 {
+
+	static void str() {
+		System.out.println("@@@@@@@@@@");
+	}
+	
+	public static void main(String[] args) {
+		
+		for(int i=0; i<3;i++) {
+			if(i==0) {
+				System.out.println("frist");
+				str();
+			}
+			else if(i==1) {
+				System.out.println("second");
+				str();
+			}else {
+				System.out.println("third");
+				str();
+			}
+		}
+	}
+
+}
