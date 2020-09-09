3906bbe2ce6cad5af6d5d4077348e964d258cb2a
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-09-09 13:36:39 +0900

    라봉
    라봉

---
+++ year2020/month09/day0909/Labong_09_09.java
@@ -0,0 +1,27 @@
+package september;
+
+import java.util.Scanner;
+
+public class Labong_09_09 {
+	
+	static void gogodan(int[] gogodan) {
+		for(int k=gogodan[0]; k<=gogodan[1];k++) {
+			System.out.println("== "+k+"dan ==");
+			for(int k2=1; k2<=9; k2++) {
+				System.out.println(k+" * "+k2+" = "+(k*k2));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] num = new int[2];
+		for(int i=0; i<2;i++) {
+			num[i] = sc.nextInt();
+		}
+		gogodan(num);
+	}
+
+}
