af05203520afcb5312262aab8874a6c0bd8a52d7
금강누나 <52662748+itkk0306@users.noreply.github.com>
2020-09-09 23:23:07 +0900

    괴구단_0909

---
+++ year2020/month09/day0909/goe_0909.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class goe_0909 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2;
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        gugu(n1,n2);
+
+        sc.close();
+    }
+
+    public static void gugu(int n1, int n2){
+
+            for (int i = n1; i <= n2; i++) {
+                System.out.println("== " + n1+ "dan" + " ==" );
+                for(int j = 1; j <= 9; j++) {
+                    System.out.println( i + " * " + j  + " =  " + i*j);
+                }
+                System.out.println();
+                n1++;
+            }
+    }
+}
