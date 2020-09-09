c6116c5f48e59fd9b15771b6773a8310a55fb13d
Lampard <45754698+JWooni@users.noreply.github.com>
2020-09-09 12:13:07 +0900

    방쬬

---
+++ year2020/month09/day0910/bangJjo_0910.java
@@ -0,0 +1,20 @@
+package question.month09.day0910;
+
+public class bangJjo_0910 {
+
+	public static void print() {
+		System.out.println("@@@@@@@@@@");
+	} // print
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first");
+		print();
+		System.out.println("second");
+		print();
+		System.out.println("third");
+		print();
+		
+	} // main
+
+} // class
