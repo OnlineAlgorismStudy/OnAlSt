5820501bd26c2161512448fa96003ebb942c3fc9
금강누나 <52662748+itkk0306@users.noreply.github.com>
2020-09-11 02:19:58 +0900

    괴_0910

---
+++ year2020/month09/day0910/goe_0910.java
@@ -0,0 +1,13 @@
+public class goe_0910 {
+    public static void main(String[] args) {
+
+        print("first");
+        print("second");
+        print("third");
+    }
+
+    public static void print(String seq) {
+        System.out.println(seq);
+        System.out.println("@@@@@@@@@@");
+    } // print
+}
