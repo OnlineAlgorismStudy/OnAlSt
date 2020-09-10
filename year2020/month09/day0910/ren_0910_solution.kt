6a0baf73ba61427b393278a4c7798e19403ed565
Ren <67572250+i-am-ren@users.noreply.github.com>
2020-09-10 20:36:13 +0900

    ren

---
+++ year2020/month09/day0910/ren_0910_solution.kt
@@ -0,0 +1,22 @@
+package week11
+
+import java.util.*
+import kotlin.math.pow
+
+
+fun main(){
+
+    fun solution(){
+        print("@".repeat(10))
+    }
+
+    println("first")
+    solution()
+    println()
+    println("second")
+    solution()
+    println()
+    println("third")
+    solution()
+
+}
