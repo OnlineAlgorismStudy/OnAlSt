7dd1464484d3a45c44d7f0f6322cbb5483fe505b
Lampard <45754698+JWooni@users.noreply.github.com>
2020-09-11 12:05:58 +0900

    방쬬

---
+++ year2020/month09/day0914/bangJjo_0914.java
@@ -0,0 +1,31 @@
+package question.month09.day0914;
+
+import java.util.Scanner;
+
+public class bangJjo_0914 {
+
+	public static void NumQuadra(int num) {
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= num; j++ ) {
+				System.out.print(i*j+" ");
+			} // end for
+			System.out.println();
+		} // end for
+		
+	} // NumQuadra
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		sc.close();
+		
+		NumQuadra(num);
+		
+	} // main
+
+} // class
