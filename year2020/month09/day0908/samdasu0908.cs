2fa60d6eedb7fc20a6c12af87c1b494025532117
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 01:52:41 +0900

    3ㄷr수

---
+++ year2020/month09/day0908/samdasu0908.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace samdasu_0908
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            fn_compare(a, b);
+        }
+        static void fn_compare(int a, int b)
+        {
+            if (a > b)
+            {
+                var quo = a / 2;
+                var odd = b * 2;
+                Console.WriteLine("{0},{1}", quo, odd);
+
+            }
+            else if (b > a)
+            {
+                var quo = b / 2;
+                var odd = a * 2;
+
+                Console.WriteLine("{0},{1}", quo, odd);
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("서로 다른 두 수를 입력해 주세요.");
+            }
+        }
+    }
+}
