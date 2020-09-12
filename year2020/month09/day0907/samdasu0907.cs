538db564b562e5b1e2d99a36e078209af2d515da
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 01:52:04 +0900

    3ektn

---
+++ year2020/month09/day0907/samdasu0907.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace samdasu_0907
+{
+    class samdasu0907
+    {
+        static void fn_calculation(double a, double b)
+        {
+            /*double val = 1;
+            for(int i = 0; i<=b; i++)
+            {
+                val = a * val;
+            }
+            return val;*/
+
+            var result =Math.Pow(a, b);
+
+            Console.WriteLine(result);
+
+            
+        }
+        static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());            
+
+            Console.WriteLine("{0},{1}",a, b);
+            fn_calculation(a,b);        
+        }
+
+        
+
+
+    }
+    
+}
