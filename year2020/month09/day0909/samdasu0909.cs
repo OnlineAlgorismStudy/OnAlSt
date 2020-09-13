2fecfe727ce099a7235ac1a3b696eee82ce9eddc
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 20:57:27 +0900

    3다수

---
+++ year2020/month09/day0909/samdasu0909.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace samdasu_0909
+{
+    class samdasu0909
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            fn_gugu(a, b);
+
+        }
+        static void fn_gugu(int a, int b)
+        {
+            if (a < b)
+            {
+                for(int i = a; i<=b; i++)
+                {
+                    Console.WriteLine("== {0}dan ==", i);
+                    for(int j = 1; j<=9; j++)
+                    {
+                        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+            else if (a > b)
+            {
+                for(int i = b; i<=a; i++)
+                {
+                    Console.WriteLine("=="+i+"dan==");
+                    for (int j = 1; j <= 9; j++)
+                    {
+                        Console.WriteLine("{0}* {1} = {2}", i, j, i * j);
+                    }
+                    Console.WriteLine(" ");
+
+                }
+            }
+        }
+    }
+}
