da4eec13674426f27762b12c73bf9b2962b845b9
Lampard <45754698+JWooni@users.noreply.github.com>
2020-09-13 19:22:51 +0900

    방쬬

--- year2020/month09/day0917/bangJjo_0917.java
+++ year2020/month09/day0917/bangJjo_0917.java
@@ -5,40 +5,43 @@ import java.util.Scanner;
 public class bangJjo_0917 {
 
 	public static void print(int[] arr) {
-		
+
 		int temp = 0;
-		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr.length; j++) {
-				if(arr[i] > arr[j]) {
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
 					temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
 				} // end if
 			} // end for
 		} // end for
-		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]+" ");
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		} // end for
-		
+
 	} // print
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int num = sc.nextInt();
-		
-		int[] arr = new int[num];
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		} // end for
-		
-		print(arr);
-		
+
+		if (num <= 10) {
+
+			int[] arr = new int[num];
+
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+			} // end for
+
+			print(arr);
+
+		} // end if
 	} // main
 
 } // class
