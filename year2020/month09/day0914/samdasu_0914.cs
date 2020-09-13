b0f1b5aeba0af42778e5e9ea3605185001689eef
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 21:12:47 +0900

    3333다수

---
+++ year2020/month09/day0914/samdasu_0914.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu_0917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            if(input <= 100)
+            {
+                fn_rectangle(input);
+            }
+            
+        }
+        static void fn_rectangle(int a)
+        {
+            for(int i = 1; i<=a; i++)
+            {
+                for(int j =1; j<=a; j++)
+                {
+                    Console.Write("{0} ", i * j);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
