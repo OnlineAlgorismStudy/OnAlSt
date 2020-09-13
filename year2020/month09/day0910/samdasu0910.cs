e8b9403855bafad91fe6d3e3d907fca51ad30e52
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 20:57:58 +0900

    33다수

---
+++ year2020/month09/day0910/samdasu0910.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace samdasu_0910
+{
+    class samdasu0910
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first");
+            fn_zero();
+            Console.WriteLine("second");
+            fn_zero();
+            Console.WriteLine("third");
+            fn_zero();
+        }
+        static void fn_zero()
+        {
+            Console.WriteLine("@@@@@@@@@@");
+        }
+    }
+}
