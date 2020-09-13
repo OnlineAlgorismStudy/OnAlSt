47cf78d5f6cc96ad018947e873f7dbe49172c48f
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-13 20:58:37 +0900

    333다수

---
+++ year2020/month09/day0911/samdasu0911.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace samdasu_0911
+{
+    class samdasu0911
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input < 1001)
+            {
+                fn_Isometric(input);
+            }
+            else { Console.WriteLine("1000이하의 자연수를 입력해 주세요"); }
+
+        }
+        static void fn_Isometric(int a)
+        {
+            int n = (a * (1 + a)) / 2;
+            Console.WriteLine(n);
+        }
+    }
+}
