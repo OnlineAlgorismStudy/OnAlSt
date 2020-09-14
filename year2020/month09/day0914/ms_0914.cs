653ca46118b8908ee3666ac94e1f04d288d478a2
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-09-14 16:09:33 +0900

    민석스

---
+++ year2020/month09/day0914/ms_0914.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace algorithm_56
+{
+    class ms_0914
+    {
+        static void printRec(int n) 
+        {
+            for(int i = 1; i <= n; i++) 
+            {
+                for(int j = 1; j <= n; j++) 
+                {
+                    Console.Write(i*j);
+
+                    if(j != n)
+                        Console.Write(' ');
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            printRec(n);
+        }
+    }
+}
