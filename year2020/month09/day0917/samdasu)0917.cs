dc0ba007fa1ec0c96d290475e00eca4fe7f32af2
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-21 21:49:43 +0900

    삼다ㅜ_0917

---
+++ year2020/month09/day0917/samdasu)0917.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace samdasu_0917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = null;
+            int input = 0;
+
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input < 11)
+            {
+                n = new int[input];
+            }
+
+            for (int i = 0; i<n.Length; i++)
+            {
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            fn_result(n);
+        }
+
+        static void fn_result(int[] vs) 
+        {
+            Array.Sort(vs);
+
+            for(int i =vs.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(vs[i] + " ");
+            }
+        }
+    }
+}
