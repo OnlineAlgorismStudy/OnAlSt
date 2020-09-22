1fffdf51b4a317648f896aad272c20e8b90b5f16
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-09-22 13:38:58 +0900

    Create Jjingu_0922.py

---
+++ year2020/month09/day0922/Jjingu_0922.py
@@ -0,0 +1,9 @@
+#찐구 0922 문제풀이
+
+def function_0922(a,b):
+    print(f"({a} - {b}) ^ 2 = {(a-b)**2}")
+    print(f"({a} + {b}) ^ 3 = {(a+b)**3}")
+    
+a, b = list(map(int,input().split(" ")))
+
+function_0922(a, b)5
