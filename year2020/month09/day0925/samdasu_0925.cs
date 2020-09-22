d20dc096dd077a28b37c096a746583a4600ddd6c
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-22 22:40:36 +0900

    삼다수0925

---
+++ year2020/month09/day0925/samdasu_0925.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _0925
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (0 < input&& input < 21)
+            {
+                fn_Pow(input);
+            }
+
+            
+        }
+
+        static void fn_Pow(int a)
+        {
+            //int value = 2;
+            Console.WriteLine(Math.Pow(2, a));
+        }
+    }
+}
