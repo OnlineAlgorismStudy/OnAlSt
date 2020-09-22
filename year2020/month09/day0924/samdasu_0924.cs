d51eac4b80561836c7a274278c24bdab25df73b1
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-22 22:11:02 +0900

    삼다수0924

---
+++ year2020/month09/day0924/samdasu_0924.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace samdasu_0924
+{
+    class samdasu_0924
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[5];
+            for(int i = 0; i<5; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            fn_AbsSum(input);
+        }
+        static void fn_AbsSum(int[] a)
+        {
+
+            int n = 0;
+            for(int i = 0; i<a.Length; i++)
+            {
+                
+
+                a[i] = Math.Abs(a[i]);
+
+                n+= a[i];
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
