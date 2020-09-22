f039954cbbda94c13f45af3062f7b4dad698421f
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-22 21:41:21 +0900

    삼다수0922

---
+++ year2020/month09/day0922/samdasu_0922.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace samdasu_0922
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[2];
+
+            for(int i = 0; i<2; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            fn_Macro(input);
+        }
+
+        static void fn_Macro(int[] a)
+        {
+            Console.WriteLine("({0}-{1}) ^ 2 = {2}", a[0], a[1], (a[0] - a[1]) * (a[0] - a[1]));
+            Console.WriteLine("({0}+{1}) ^ 3 = {2}", a[0], a[1], (a[0] + a[1]) * (a[0] + a[1])*(a[0] + a[1]));
+
+        }
+    }
+}
