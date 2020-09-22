141259ed38e621394a97062c687d7feed77ed8ff
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-09-22 22:03:28 +0900

    삼다수0923

---
+++ year2020/month09/day0923/samdasu_0923.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace samdasu_0923
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = new double[2];
+
+            for(int i = 0; i<2; i++)
+            {
+                input[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            fn_root(input);
+        }
+
+        static void fn_root(double[] a)
+        {
+            int n = 0;
+
+            var root1 =Math.Sqrt(a[0]);
+            //Console.WriteLine(root1);
+            var root2 = Math.Sqrt(a[1]);
+
+            double bigger = (root1 > root2 ? root1 : root2);
+            double smaller = (root1 > root2 ? root2 : root1);
+
+            bigger = Math.Floor(bigger);
+            smaller = Math.Ceiling(smaller);
+
+            while (smaller <= bigger)
+            {
+                smaller++;
+                n++;
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
