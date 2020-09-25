cefaadb4f7c075b6db628516cf1b6e3e85012b0b
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-09-25 09:24:08 +0900

    Update pot_0925.java

--- year2020/month09/day0925/pot_0925.java
+++ year2020/month09/day0925/pot_0925.java
@@ -2,14 +2,14 @@ package study;
 import java.util.Scanner;
 
 public class st0925 {
-	public static void cal(double num) {
-		double result = Math.pow(2.0, num);
-		System.out.println((int)result);
+	public static void cal(int num) {
+		int result = (int)Math.pow(2, num);
+		System.out.println(result);
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double num = sc.nextDouble();
+		int num = sc.nextInt();
 		cal(num);
 	}
 
