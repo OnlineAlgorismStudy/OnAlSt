9b5fe3a2e9c68cd4cfa733b3fcfd1962fd249df6
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-09-27 22:14:03 +0900

    2020.09.27 : upload algo by Y

---
+++ year2020/month10/day1005/Y_1005.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define PI 3.141592
+using namespace std;
+void print(int n, int sum);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    print(n, 0);
+}
+
+void print(int n, int sum)
+{
+    if(n == 0)
+    {
+        printf("%d\n", sum);
+        return;
+    }
+    
+    print(n-1, sum+n);
+}
