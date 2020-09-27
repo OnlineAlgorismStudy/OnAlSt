3afa2b7511e5d19339d423bf096d6748a3f8f047
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-09-27 22:20:52 +0900

    2020.09.27 : upload algo by Y

---
+++ year2020/month10/day1006/Y_1006.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define PI 3.141592
+using namespace std;
+int print(int n);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", print(n));
+}
+
+int print(int n)
+{
+    if(n<=1) return 1;
+    return print(n/2) + print(n-1);
+}
