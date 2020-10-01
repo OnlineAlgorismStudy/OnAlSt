16144b1156e8ae20596de15c409a39834586180a
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-10-01 11:04:46 +0900

    민석스

---
+++ year2020/month10/day1001/ms_1001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace algorithm_69
+{
+    class ms_1001
+    {
+        static void recursionPrint(int n) 
+        {
+            if(n == 0)
+                return;
+            
+            Console.WriteLine("recursive");
+            recursionPrint(n-1);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if(0 < n && n <= 20) 
+                recursionPrint(n);
+        }
+    }
+}
