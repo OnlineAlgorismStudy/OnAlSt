97778e66969261059675ef22b9701265d26a70b3
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-02 17:20:30 +0900

    3다수 1002

---
+++ year2020/month10/day1002/samdasu1002.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace samdasu1002
+{
+    class samdasu1002
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input <= 50)
+            {
+                fn_Recursion(input);
+            }
+        }
+        static void fn_Recursion(int a)
+        {
+            if(a == 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.Write(a + " ");
+                a--;
+                fn_Recursion(a);
+            }
+        }
+    }
+}
