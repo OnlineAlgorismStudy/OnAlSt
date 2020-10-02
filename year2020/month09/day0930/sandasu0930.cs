fdfbb3d0b30ab7d395efb3dc15881ec19ef9aa7a
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-02 17:19:41 +0900

    삼다수 0930

---
+++ year2020/month09/day0930/sandasu0930.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace samdasu_0930
+{
+    class sandasu0930
+    {
+        const double py = 3.141592;
+        static void Main(string[] args)
+        {
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            fn_circle(input);
+        }
+
+        static void fn_circle(double a)
+        {
+            Console.WriteLine("{0:0.000}",a * a * py);
+        }
+    }
+}
