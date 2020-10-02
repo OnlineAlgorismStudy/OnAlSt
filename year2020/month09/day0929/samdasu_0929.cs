51d96c616c63df2cc93f9fb52ce028d4a209f3fc
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-02 17:19:12 +0900

    3다수 0929

---
+++ year2020/month09/day0929/samdasu_0929.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace samdasu_0929
+{
+    class samdasu_0929
+    {
+        const int CNT = 7;
+        static void Main(string[] args)
+        {
+            int[] input = new int[7];
+            for(int i =0; i < 7; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            fn_sort(input);
+        }
+
+        static void fn_sort(int[] a)
+        {
+            int n = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        n = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = n;
+                    }
+                }
+            }
+
+            for(int k = 0; k<a.Length; k++)
+            {
+                Console.Write(a[k] + " ");
+            }
+            
+        }
+    }
+}
