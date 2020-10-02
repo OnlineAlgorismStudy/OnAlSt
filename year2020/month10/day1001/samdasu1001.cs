d41c419c7f7f2a1a33a6ee678bbc63b0e1b97cab
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-02 17:20:05 +0900

    3다수 1001

---
+++ year2020/month10/day1001/samdasu1001.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace samdasu1001
+{
+    class samdasu1001
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input <= 20)
+            {
+                fn_Recursion(input);
+            }
+        }
+        static void fn_Recursion(int a)
+        {
+            if(a == 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("recursive");
+                a--;
+                fn_Recursion(a);
+            }
+        }
+    }
+}
