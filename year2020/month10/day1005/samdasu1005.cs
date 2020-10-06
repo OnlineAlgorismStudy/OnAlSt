5343880b3c688b51bc143da5bcedb4c0890677aa
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-06 19:07:01 +0900

    삼 1005

---
+++ year2020/month10/day1005/samdasu1005.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace samdaus_1005
+{
+    class Program
+    {
+        static int sum = 0;
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (0 < n && n<= 100)
+            {
+                fn_nSum(n);
+            }
+        }
+        static void fn_nSum(int a)
+        {
+            if (a == 0)
+            {
+                Console.WriteLine(sum);
+                return;
+            }
+            else
+            {
+                sum += a;
+                a--;
+                fn_nSum(a);
+            }
+        }
+    }
+}
