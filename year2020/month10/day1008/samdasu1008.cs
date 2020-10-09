196f8a25cd6ab10516d56755e1373dc155c82e09
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-09 17:44:50 +0900

    삼다수 1008

---
+++ year2020/month10/day1008/samdasu1008.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu1008
+{
+    class samdasu1008
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            fn_n(input);
+        }
+
+        static void fn_n(int a)
+        {
+            if (a == 0)
+            {
+                return;
+            }
+            else
+            {
+                fn_n(a / 2);
+
+                Console.Write(a + " ");
+            }
+        }
+    }
+}
