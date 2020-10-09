859d76f741dece8edff65796550c24c103d88bd7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-09 17:43:09 +0900

    3더ㅏ수

---
+++ year2020/month10/day1006/samdasu1006.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace sam1006
+{
+    class samdasu1006
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+
+            if(input <= 50)
+            {
+                fn_Recursion(input);
+
+                Console.WriteLine(fn_Recursion(input));
+            }
+        }
+
+        static int fn_Recursion(int a)
+        {
+            if(a == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fn_Recursion(a / 2) + fn_Recursion(a - 1);
+            }
+        }
+    }
+}
