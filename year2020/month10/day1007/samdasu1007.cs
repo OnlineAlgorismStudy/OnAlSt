5ccc8ddc388738001e64b314c6e2d4f2b0ce3520
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-09 17:44:22 +0900

    삼다수 1007

---
+++ year2020/month10/day1007/samdasu1007.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace samdasu_1007
+{
+    class samdasu1007
+    {
+        static int sum = 0;
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            fn_n(input);
+
+            Console.WriteLine(fn_n(input));
+
+        }
+        static int fn_n(int a)
+        {
+            if (a==0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (Convert.ToInt32(Math.Pow(a%10,2)) + fn_n(Convert.ToInt32(a / 10)));
+
+                /*int b = a.ToString().Length;
+                sum += b * b;
+                return fn_n(sum);*/
+            }
+        }
+    }
+}
