274d8362202406c38ee9bad0205eea3772925ce7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-09 17:45:40 +0900

    3다수 1009

---
+++ year2020/month10/day1009/samdasu1009.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu1009
+{
+    class samdasu1009
+    {
+        static void Main(string[] args)
+        {
+            //int input = 0;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            fn_f(input);
+        }
+        static void fn_f(int a)
+        {
+            if(a <= 0)
+            {
+                
+            }
+            else
+            {
+                fn_f(a - 2);
+
+                Console.Write(a + " ");
+            }
+        }
+    }
+}
