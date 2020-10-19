5f45b896fcb3e314edd763140ae4f9592990e9c7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-19 14:10:09 +0900

    3다수 1013

---
+++ year2020/month10/day1013/samdasu1013.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu_1013
+{
+    class samdasu1013
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(fn_n(input));
+        }
+
+        static int fn_n(int a)
+        {
+            if (a <= 2)
+            {
+
+                return a;
+            }
+
+            
+            return((fn_n(a - 1) * fn_n(a - 2)) % 100);
+
+        }
+    }
+}
