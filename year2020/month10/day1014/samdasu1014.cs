cbf6fa0f8166b4ee9280254c8d24db34871ad283
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-19 14:10:38 +0900

    삼다수1014

---
+++ year2020/month10/day1014/samdasu1014.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace samdasu1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            fn_f(input,0);
+        }
+
+        static void fn_f(int a, int b)
+        {
+            if(a == 1)
+            {
+                Console.WriteLine(b);
+                
+            }
+            else if(a%2 == 0)
+            {
+                fn_f(a / 2, b + 1);
+            }
+            else
+            {
+                fn_f(a / 3, b + 1);
+            }
+        }
+    }
+}
