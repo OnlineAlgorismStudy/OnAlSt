61475704876af1acd40ebe7c638d56e79904c5da
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-10-20 22:39:33 +0900

    라봉
    라봉

---
+++ year2020/month10/day1021/Labong_10_21.java
@@ -0,0 +1,12 @@
+package october;
+
+public class Labong_10_21 {
+
+	public static void main(String[] args) {
+		
+		String m = "Hong Gil Dong";
+		
+		System.out.println(m.substring(3,7));
+	}
+
+}
