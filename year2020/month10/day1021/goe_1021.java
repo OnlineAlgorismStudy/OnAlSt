73e43838af2568c9bdf0df15383d4d8939661f7f
킹강 <52662748+itkk0306@users.noreply.github.com>
2020-10-22 13:31:06 +0900

    괴길동_1021

---
+++ year2020/month10/day1021/goe_1021.java
@@ -0,0 +1,9 @@
+public class goe_1021 {
+    public static void main(String[] args) {
+
+        String gildong = "Hong Gil Dong";
+
+        for(int i = 3; i <= 6; i++)
+        System.out.print(gildong.charAt(i));
+    }
+}
