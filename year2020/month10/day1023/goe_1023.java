90a036ea5a47157913b043bdd9fb1ccead3f7557
킹강 <52662748+itkk0306@users.noreply.github.com>
2020-10-23 07:57:32 +0900

    괴_1023

---
+++ year2020/month10/day1023/goe_1023.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class goe_1023 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.println(str1.length() + str2.length());
+
+        sc.close();
+    }
+}
