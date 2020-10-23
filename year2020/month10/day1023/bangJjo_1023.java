daa7952103a55bb3dbe0866032d062625c46d974
Lampard <45754698+JWooni@users.noreply.github.com>
2020-10-23 23:43:14 +0900

    방쬬

---
+++ year2020/month10/day1023/bangJjo_1023.java
@@ -0,0 +1,21 @@
+package question.month10.day1023;
+
+import java.util.Scanner;
+
+public class bangJjo_1023 {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String input1 = sc.next();
+
+		String input2 = sc.next();
+
+		sc.close();
+		
+		System.out.println(input1.length() + input2.length());
+
+	} // main
+
+} // class
