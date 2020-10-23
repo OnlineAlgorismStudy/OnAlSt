42a4f6170679680600802f1c10b7271bfc2b6025
Lampard <45754698+JWooni@users.noreply.github.com>
2020-10-23 23:36:32 +0900

    방쬬

---
+++ year2020/month10/day1021/bangJjo_1021.java
@@ -0,0 +1,19 @@
+package question.month10.day1021;
+
+public class bangJjo_1021 {
+
+	public static void main(String args[]) {
+
+		String input = "Hong Gil Dong";
+
+		String[] change = input.split("");
+
+		for (int i = 3; i <= 6; i++) {
+
+			System.out.print(change[i]);
+
+		} // end for
+
+	} // main
+
+} // class
