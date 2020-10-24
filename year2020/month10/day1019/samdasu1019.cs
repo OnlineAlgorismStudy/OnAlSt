63ebfb079751a7af5b1f373eef681492afc903a7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 19:05:39 +0900

    삼다수1019

---
+++ year2020/month10/day1019/samdasu1019.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace samdasu_1019
+{
+    class samdasu1019
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.Write("ASCII code =? ");
+
+                //ArrayList arr = new ArrayList();
+                //arr.Add(Convert.ToInt16(Console.ReadLine()));
+                int n = 0;
+                n = Convert.ToInt32(Console.ReadLine());
+
+                
+                if(n < 65 || n > 122)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToChar(n));
+                }
+                
+                
+                
+            }
+        }
+    }
+}
