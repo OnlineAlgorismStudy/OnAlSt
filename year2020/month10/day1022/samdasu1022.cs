32b771fb479abfff24a4860b060b0a62bece170c
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 19:07:31 +0900

    삼다수 1022

---
+++ year2020/month10/day1022/samdasu1022.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace samdasu_1022
+{
+    class samdasu1022
+    {
+        static void Main(string[] args)
+        {
+            string str_input = Console.ReadLine();
+            int int_input = Convert.ToInt32(Console.ReadLine());
+
+
+            for(int i = 0; i<int_input; i++)
+            {
+                Console.Write(str_input[str_input.Length - i - 1]);
+            }
+            
+        }
+    }
+}
