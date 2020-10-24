410fc48c45e49a5709d8f055db8061d2eccddb09
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 19:08:11 +0900

    삼다수 1023

---
+++ year2020/month10/day1023/samdasu1023.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace samdasu1023
+{
+    class samdasu1023
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            if(input1.Length<21 && input2.Length < 21)
+            {
+                Console.WriteLine(input1.Length + input2.Length);
+            }
+        }
+    }
+}
