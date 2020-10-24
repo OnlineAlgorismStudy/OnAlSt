0d18af4b703cbfecec0e9d918d925fbf5734eab9
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 19:06:52 +0900

    삼다수 1021

---
+++ year2020/month10/day1021/samdasu1021.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace samdaus_1021
+{
+    class samdasu1021
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hong Gil Dong";
+
+            Console.WriteLine(str.Substring(3, 4));
+        }
+    }
+}
