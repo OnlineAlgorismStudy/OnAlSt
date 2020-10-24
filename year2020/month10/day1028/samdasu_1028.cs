02a7e6bddb0706a6cc568102507d3bf6c7a86c19
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-25 00:53:09 +0900

    3다수 1028

---
+++ year2020/month10/day1028/samdasu_1028.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SAMDASU_1028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(input.Length);
+        }
+    }
+}
