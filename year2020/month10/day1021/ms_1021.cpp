576a7260e7126e3779fc052b09cadb6154c510ae
Ellimis <67461878+Ellimis@users.noreply.github.com>
2020-10-25 03:50:35 +0900

    민석스!!!

---
+++ year2020/month10/day1021/ms_1021.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	string name = "Hong Gil Dong";
+
+	cout << name.substr(3, 4) << endl;
+
+	return 0;
+}
---
+++ year2020/month10/day1021/ms_1021.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace algorithm_83
+{
+    class ms_1021
+    {
+        static void Main(string[] args)
+        {
+            String name = "Hong Gil Dong";
+
+            Console.WriteLine(name[3..7]);
+        }
+    }
+}
