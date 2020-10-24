c8dd853ad047bca03b49aad653706d746f8cabcc
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 19:06:11 +0900

    삼다수1020

---
+++ year2020/month10/day1020/samdasu1020.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace samdasu_1020
+{
+    class samdasu1020
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if(input.Length < 101)
+            {
+                Console.WriteLine(input + input);
+            }
+        }
+    }
+}
