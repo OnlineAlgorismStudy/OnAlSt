959710aea293c6f55184b057bd94aeb48d7b7e5c
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-25 00:19:06 +0900

    삼다수 1026

---
+++ year2020/month10/day1026/samdasu1026.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace samdasu_1016
+{
+    class samdasu1026
+    {
+        static void Main(string[] args)
+        {
+            
+
+            while (true)
+            {
+                var input = Convert.ToChar(Console.ReadLine());
+
+                if (input >=65 && input <= 90)
+                {
+                    Console.WriteLine(input);
+                }
+                else if(input >= 97 && input <= 122)
+                {
+                    Console.WriteLine(input);
+                }
+                else if(input >= 48 && input <= 57)
+                {
+                    Console.WriteLine(Convert.ToInt32(input));
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
