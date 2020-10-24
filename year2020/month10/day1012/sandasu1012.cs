2c4e77cf9bf81261bd4b7f5d52d4c91bb20ea547
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-10-24 23:35:22 +0900

    삼다수 1012 갸아아악

---
+++ year2020/month10/day1012/sandasu1012.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace samdasu_1012
+{
+    /*
+     *  0<n<7
+     *  1 2 x
+     *  1 3 6 o
+     */
+    class sandasu1012
+    {
+
+        static int[] arr = new int[10];
+        static int v = 0;
+
+        static void Main(string[] args)
+        {
+            /*int input_1 = Convert.ToInt32(Console.ReadLine());
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+
+*/
+
+            string[] input = Console.ReadLine().Split(' ');
+            int input_1 = Convert.ToInt32(input[0]);
+            int input_2 = Convert.ToInt32(input[1]);
+
+
+
+
+
+            fn_f(input_1, input_2);
+        }
+
+        static void fn_f(int a, int b)
+        {
+            
+
+            for (int i = 1; i<7; ++i)
+            {
+                arr[v] = i;
+                if(v == (a - 1))
+                {
+                    int temp = 0;
+                    for(int j = 0; j<v+1; ++j)
+                    {
+                        temp+=arr[j];
+                    }
+
+                    if(temp == b)
+                    {
+                        for(int k = 0; k<a; ++k)
+                        {
+                            Console.Write(arr[k] + " ");
+                        }
+                        Console.WriteLine(" ");
+                    }
+                }
+                else
+                {
+                    v++;
+                    fn_f(a, b);
+                    v--;
+                }
+            }
+            
+        }
+    }
+}
