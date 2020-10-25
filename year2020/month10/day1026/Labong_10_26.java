b788b16e57bba9b15107e09f0c07f6165e8d201f
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-10-25 18:41:14 +0900

    라봉
    랍홍

---
+++ year2020/month10/day1026/Labong_10_26.java
@@ -0,0 +1,19 @@
+package october;
+
+import java.util.Scanner;
+
+public class Labong_10_26 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char scan = sc.next().charAt(0);
+	
+		System.out.print(scan >= 48 && scan <= 57 ? scan : "");
+		
+		System.out.print(scan >= 65 && scan <= 90 ? scan : "");
+		
+		System.out.print(scan >= 97 && scan <= 122 ? scan : "");
+	}
+}
