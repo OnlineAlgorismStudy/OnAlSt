8e09a6e6f6b5ed34bd70b5cf621b8a05e841c96c
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-10-26 19:28:13 +0900

    Create Jjingu_1026.py

---
+++ year2020/month10/day1026/Jjingu_1026.py
@@ -0,0 +1,12 @@
+ 찐구 1026 문제풀이
+
+repeat = True
+while repeat:
+    mystring = input()
+    if mystring.isalpha():
+        print(mystring)
+    elif mystring.isdigit():
+        print(chr(int(mystring)))
+    else:
+        break
+ 
