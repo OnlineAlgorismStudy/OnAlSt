ddff4347ff1b261334f4db873e6fd16331515194
HS Song <62323121+hansong0219@users.noreply.github.com>
2020-10-26 19:35:20 +0900

    Create Jjingu_1027.py

---
+++ year2020/month10/day1027/Jjingu_1027.py
@@ -0,0 +1,9 @@
+Jjingu 1027 Solution
+
+mystring = input()
+trans = ""
+for i in range(len(mystring)):
+    if mystring[i].isalpha():
+        trans += mystring[i].upper()
+                
+print(trans)
