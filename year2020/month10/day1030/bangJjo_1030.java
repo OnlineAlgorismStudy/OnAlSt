59c1251c7bbf415c13d52e9b162e6d386fe68ea8
Lampard <45754698+JWooni@users.noreply.github.com>
2020-10-29 18:03:51 +0900

    방쬬

--- year2020/month10/day1030/bangJjo_1030.java
+++ year2020/month10/day1030/bangJjo_1030.java
@@ -3,27 +3,34 @@ package question.month10.day1030;
 import java.util.Scanner;
 
 public class bangJjo_1030 {
-
-	public static void main(String args[]) {
-
-		Scanner sc = new Scanner(System.in);
-
-		char input1 = sc.next().charAt(0);
-
-		char input2 = sc.next().charAt(0);
-
-		int result = (int) input1 - (int) input2;
-
-		System.out.print(((int) input1 + (int) input2) + " ");
-
-		if (result < 0) {
-
-			System.out.print(-result);
-		} else {
-
-			System.out.print(result);
-		} // end if
-		sc.close();
-	} // main
-
+ 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String input = sc.nextLine();
+ 
+        String[] arr = input.split("");
+ 
+        for (int i = 0; i < arr.length; i++) {
+ 
+            char temp = input.charAt(i);
+ 
+            if (temp >= 65 && temp <= 90) {
+ 
+                System.out.print((char) (temp + 32));
+ 
+            } else if (temp >= 97 && temp <= 122) {
+ 
+                System.out.print(temp);
+            } else if (temp >= 48 && temp <= 57) {
+ 
+                System.out.print(temp);
+            } // end if
+ 
+        } // end for
+        sc.close();
+    } // main
+ 
 } // class
