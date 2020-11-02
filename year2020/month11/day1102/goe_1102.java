ab61292d5fc583586e206040fb559f59acbe23fd
킹강 <52662748+itkk0306@users.noreply.github.com>
2020-11-02 22:34:46 +0900

    goe_1102

--- year2020/month11/day1102/goe_1102.java
+++ year2020/month11/day1102/goe_1102.java
@@ -5,8 +5,16 @@ public class goe_1102 {
 
         Scanner sc = new Scanner(System.in);
 
-        String word1 = sc.next();
-        String word2 = sc.next();
+        String word1, word2;
+
+        word1 = sc.next();
+        word2 = sc.next();
+
+        while (word1.length() > 100 || word2.length() > 100) {
+            System.out.println(" 단어의 길이는 100자 이하로 입력하시오");
+            word1 = sc.next();
+            word2 = sc.next();
+        }
 
         if(word1.length() < word2.length())
             System.out.println(word2.length());
