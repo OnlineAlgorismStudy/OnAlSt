171304e0c9ed34cb25795d573a4ae6d01cbbce69
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-11-04 21:50:26 +0900

    라봉
    라봉

---
+++ year2020/month11/day1102/Labong_11_01.java
@@ -0,0 +1,34 @@
+package november;
+
+import java.util.Scanner;
+
+public class Labong_11_01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str1 = "";
+		String str2 = "";
+		while(true) {
+			System.out.println("??");
+			str1 = sc.next();
+			str2 = sc.next();
+			if(str1.length() > 100 || str1.length() > 100) {
+				str1 = ""; str2 = "";
+				continue;
+			}else {
+				break;
+			}
+		}
+		
+		
+		if(str1.length() < str2.length()) {
+			System.out.println(str2.length());
+		}else {
+			System.out.println(str1.length());
+		}
+
+	}
+
+}
