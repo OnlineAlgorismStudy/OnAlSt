1eba53a0f1d31dbd69b368f14b93efcf5ac7b47a
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-05 00:21:41 +0900

    samdasu_1102

---
+++ year2020/month11/day1102/samdasu1102.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace samdasu1102
+{
+    class samdasu1102
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] ar_input = input.Split(' ');
+
+            string input1 = ar_input[0];
+            string input2 = ar_input[1];
+            if(input1.Length<101 && input2.Length < 101)
+            {
+                if (input1.Length > input2.Length)
+                {
+                    Console.WriteLine(input1.Length.ToString());
+                }
+                else if (input2.Length > input1.Length)
+                {
+                    Console.WriteLine(input2.Length.ToString());
+
+                }
+                else
+                {
+
+                }
+            }
+            
+        }
+    }
+}
