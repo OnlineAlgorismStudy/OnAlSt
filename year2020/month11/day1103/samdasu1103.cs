9b2e5e5815f8b31cfca476bffacd22878f2c331f
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-05 00:22:18 +0900

    삼다수1103

---
+++ year2020/month11/day1103/samdasu1103.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace samdasu1103
+{
+    class samdasu1103
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (true)
+            {
+
+                int int_input = Convert.ToInt32(Console.ReadLine());
+
+                int input_lengh = input.Length;
+
+                if(int_input>= input_lengh)
+                {
+                    int_input = input_lengh - 1;
+                }
+                else
+                {
+                    int_input -= 1;
+                }
+
+                if(input.Length == 1)
+                {
+                    break;
+                }
+                input = input.Remove(int_input, 1);
+
+                Console.WriteLine(input);
+
+
+            }
+            
+        }
+    }
+}
