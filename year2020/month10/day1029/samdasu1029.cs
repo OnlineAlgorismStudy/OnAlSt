57ddb7b478b330914b648030a5d31719e01dda20
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-05 00:19:27 +0900

    samdasu1029

---
+++ year2020/month10/day1029/samdasu1029.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu1029
+{
+    class samdasu1029
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] c_input = new char[100];
+            char temp;
+            for(int i = input.Length -1; i>=0; i--)
+            {
+                for(int j = i; j<input.Length; j++)
+                {
+                    Console.Write(input[j]);
+                }
+                for(int k = 0; k<i; k++)
+                {
+                    Console.Write(input[k]);
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
