94105a5e0b948a2d9ea0bc01294ab1c5af2ebc69
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-05 00:19:59 +0900

    samdasu1030

---
+++ year2020/month10/day1030/samdasu1030.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace samdasu1030
+{
+    class samdasu1030
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length < 101)
+            {
+                for(int i = 0; i<input.Length; i++)
+                {
+                    if((input[i]>=65 && input[i]<=90)
+                        ||(input[i])>=97 && input[i] <= 122)
+                    {
+                        Console.Write(Char.ToLower(input[i]));
+                    }
+                    else if(input[i]>= 48 && input[i] <= 57)
+                    {
+                        Console.Write(input[i]);
+                    }
+                }
+            }
+        }
+    }
+}
