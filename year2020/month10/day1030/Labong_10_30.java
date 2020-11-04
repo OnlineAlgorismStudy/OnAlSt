53aad8d4d55ce9a48b889719f926ed231a4b6e28
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-11-04 21:26:21 +0900

    라봉
    라봉

---
+++ year2020/month10/day1030/Labong_10_30.java
@@ -0,0 +1,25 @@
+package october;
+
+import java.util.Scanner;
+
+public class Labong_10_30 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str;
+		
+		while(true) {
+			str = sc.nextLine().replaceAll("[^0-9a-zA-Z]", "");
+			
+			if(str.length() >100) {
+				str = "";
+			}else {
+				str = str.toLowerCase();
+				break;
+			}
+		}
+		System.out.println(str);
+	}
+
+}
