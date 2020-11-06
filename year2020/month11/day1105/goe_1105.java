2d63ddeae81cb6f41945bdfece3d91fe3e57f5d3
킹강 <52662748+itkk0306@users.noreply.github.com>
2020-11-06 14:32:05 +0900

    괴수정_1105

--- year2020/month11/day1105/goe_1105.java
+++ year2020/month11/day1105/goe_1105.java
@@ -9,6 +9,10 @@ public class goe_1105 {
 
         for(int i = 0; i < str.length; i++){
             str[i] = sc.nextLine();
+            while (str[i].length() > 30){
+                System.out.println("30자 이하로 입력하시오");
+                str[i] = sc.nextLine();
+            }
         }
 
         for(int i = str.length; i > 0; i--){
