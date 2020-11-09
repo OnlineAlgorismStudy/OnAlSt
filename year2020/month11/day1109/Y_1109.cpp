1b8d8769f0faf639aacaf50afe99b5d85cda633d
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-11-09 17:07:45 +0900

    2020.11.09 : upload algo by Y

---
+++ year2020/month11/day1109/Y_1109.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	cout << s << "fighting" << endl;
+}
