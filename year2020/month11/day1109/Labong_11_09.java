235509bdcaafe422e3694d1734cf603dc316ef7e
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-11-14 00:20:46 +0900

    라봉
    라봉

---
+++ year2020/month11/day1109/Labong_11_09.java
@@ -0,0 +1,21 @@
+package november;
+
+import java.util.Scanner;
+
+public class Labong_11_09 {
+	
+	public static String add() {
+		
+		return"fighting";
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		System.out.println(str+add());
+	}
+
+}
