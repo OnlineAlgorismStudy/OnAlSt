160dd2fd84d6d3a13585af6e381a72c9db86f96f
Lampard <45754698+JWooni@users.noreply.github.com>
2020-11-16 18:49:19 +0900

    방쬬

---
+++ year2020/month11/day1109/bangJjo_1109.java
@@ -0,0 +1,21 @@
+package question.month11.day1109;
+
+import java.util.Scanner;
+
+public class bangJjo_1109 {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String plus = "fighting";
+
+		String input = sc.next();
+
+		System.out.println(input + plus);
+
+		sc.close();
+		
+	} // main
+
+} // class
