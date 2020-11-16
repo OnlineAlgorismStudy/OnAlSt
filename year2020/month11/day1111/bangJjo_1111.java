7dcc99b3a2842e4190aa713cc64cd9a874454b6d
Lampard <45754698+JWooni@users.noreply.github.com>
2020-11-16 19:17:39 +0900

    방쬬

---
+++ year2020/month11/day1111/bangJjo_1111.java
@@ -0,0 +1,35 @@
+package question.month11.day1111;
+
+import java.util.Scanner;
+
+public class bangJjo_1111 {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.next();
+
+		if (input.contains("c")) {
+
+			System.out.print("Yes ");
+
+		} else {
+
+			System.out.print("No ");
+
+		} // end if
+
+		if (input.contains("ab")) {
+
+			System.out.print("Yes");
+
+		} else {
+
+			System.out.print("No");
+
+		} // end if
+
+	} // main
+
+} // class
