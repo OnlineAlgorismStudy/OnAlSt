2dff5230b400afc3f17e45821322a09add69330b
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:53:54 +0900

    333

---
+++ year2020/month11/day1111/samdasu1111.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1111
+{
+    class samdasu1111
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length < 100)
+            {
+                if (input.Contains("c"))
+                {
+                    Console.Write("Yes ");
+                }                
+                else
+                {
+                    Console.Write("NO ");
+                }
+
+                if (input.Contains("ab"))
+                {
+                    Console.Write("Yes ");
+                }
+                else
+                {
+                    Console.Write("NO");
+                }
+            }
+        }
+    }
+}
