161ff27599338c32e95e0503f8cbe719e6db11a7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:51:56 +0900

    3다수

---
+++ year2020/month11/day1106/samdasu1106.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1106
+{
+    class samdasu1106
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] arr = input.Split(' ');
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
