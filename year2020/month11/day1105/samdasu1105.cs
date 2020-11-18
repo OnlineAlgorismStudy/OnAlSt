7595f6f11fb22a85eed6556840506cdcfa0e39eb
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:51:31 +0900

    삼 1105

---
+++ year2020/month11/day1105/samdasu1105.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1105
+{
+    class samdasu1105
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[5];
+
+            for(int i = 0; i<5; i++)
+            {
+                a[i] = Console.ReadLine();
+
+                
+            }
+
+            for(int i = a.Length -1; i>=0; i--)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
