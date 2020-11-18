9889839d50166cee790f19f1dc06e6c2ccfe442a
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:53:29 +0900

    3ㅏㅁ

---
+++ year2020/month11/day1110/samdasu1110.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1110
+{
+    class samdasu1110
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] a = input.Split(" ");
+
+            string eon = a[0].Substring(0,2);       //앞 두글자.
+
+            string print= eon + a[1].Substring(2,a[1].Length-2) + eon;
+
+            Console.WriteLine(print);
+        }
+    }
+}
