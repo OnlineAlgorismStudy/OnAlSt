6ab880e5970aa26bb8c9d83e9b80bb336d6a1a60
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:54:25 +0900

    3다수

---
+++ year2020/month11/day1112/samdasu1112.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            Array.Sort(input);
+
+            Console.WriteLine(input[0]);
+        }
+    }
+}
