b3a87aaf641095cd462d0542b89edb794f04016b
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:52:16 +0900

    Add files via upload

---
+++ year2020/month11/day1109/samdasu1109.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1107
+{
+    class samdasu1109
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length < 21)
+            {
+                Console.WriteLine(input + "fighting");
+            }
+            
+        }
+    }
+}
