0db53a3e4db24f6602c51da564321be99f25acde
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-18 11:55:43 +0900

    3다수

---
+++ year2020/month11/day1113/samdasu1113.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string input2 = input.Substring(0, input.IndexOf('.'));
+            int a = Convert.ToInt32(input2) * 2;
+
+            string input3 = input.Substring(0, input.IndexOf('*'));
+            double b = Convert.ToDouble(input3);
+            Console.WriteLine(a+"\n"+Math.Round(b,2)+0);
+
+
+        }
+    }
+}
