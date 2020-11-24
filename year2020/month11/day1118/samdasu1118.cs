b2c4f5b1590fc60c3a940530936e365a5f0f6ce7
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-25 00:02:03 +0900

    삼

---
+++ year2020/month11/day1118/samdasu1118.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _1118
+{
+    class samdasu1118
+    {
+        static void Main(string[] args)
+        {
+            string a = "flower";
+            string b = "rose";
+            string c = "lily";
+            string d = "daffodil";
+            string e = "azalea";
+
+
+            string aa = a.Substring(0, 3);
+            string bb = b.Substring(0, 3);
+            string cc = c.Substring(0, 3);
+            string dd = d.Substring(0, 3);
+            string ee = e.Substring(0, 3);
+
+            string input = Console.ReadLine();
+
+            int count = 0;
+
+            
+            if (aa.Contains(input))
+            {
+                Console.WriteLine(a);
+                count++;
+            }
+            if (bb.Contains(input))
+            {
+                Console.WriteLine(b);
+                count++;
+            }
+            if (cc.Contains(input))
+            {
+                Console.WriteLine(c);
+                count++;
+            }
+            if (dd.Contains(input))
+            {
+                Console.WriteLine(d);
+                count++;
+            }
+            if (ee.Contains(input))
+            {
+                Console.WriteLine(e);
+                count++;
+            }
+            if(count > 0)
+            {
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+            
+        }
+    }
+}
