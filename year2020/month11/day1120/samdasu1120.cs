4b59c06be2855b041d7f1c862bb46094910148c9
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-25 00:44:49 +0900

    3

---
+++ year2020/month11/day1120/samdasu1120.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace _1120
+{
+    class samdasu1120
+    {
+        static void Main(string[] args)
+        {
+            int intInput = Convert.ToInt32(Console.ReadLine());
+            string[] temp = new string[intInput];
+            for(int i = 0; i < intInput; i++)
+            {
+                string input = Console.ReadLine();
+
+                temp[i] = input;
+            }
+
+            Array.Sort(temp);
+
+            for(int j = 0; j<intInput; j++)
+            {
+                Console.WriteLine(temp[j]);
+            }
+        }
+    }
+}
