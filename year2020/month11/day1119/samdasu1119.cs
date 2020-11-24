2c56e21905abd23df40bd34558234298a077872b
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-25 00:44:25 +0900

    33

---
+++ year2020/month11/day1119/samdasu1119.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace _1119
+{
+    class samdasu1119
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arInput = new ArrayList();
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                
+
+                if (input.Equals("0"))
+                {
+                    break;
+                }
+                else
+                {
+                    arInput.Add(input);
+                }
+            }
+
+            
+            Console.WriteLine(arInput.Count);
+            for (int i = 0; i < arInput.Count; i = i + 2)
+            {
+                Console.WriteLine(arInput[i]);
+            }
+
+
+        }
+    }
+}
