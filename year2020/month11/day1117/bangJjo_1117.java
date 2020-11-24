e56286d707bfc6c2be3e6c6b17669d8c9714a546
Lampard <45754698+JWooni@users.noreply.github.com>
2020-11-24 12:08:22 +0900

    방쬬

---
+++ year2020/month11/day1117/bangJjo_1117.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+
+		String[] arr = input.split(" ");
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+
+			System.out.println(arr[i]);
+
+		} // end for
+		sc.close();
+	} // main
+
+} // class
