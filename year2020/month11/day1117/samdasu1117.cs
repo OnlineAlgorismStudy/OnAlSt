97ad75b3e926cbdfe860546b3d38ba54a3145269
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-25 00:01:12 +0900

    삼

---
+++ year2020/month11/day1117/samdasu1117.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1117
+{
+    class samdasu1117
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+
+            for(int i = input.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(input[i].ToString());
+            }
+        }
+    }
+}
