f3c56d04f3f80542eae4de834f61c78c8cbbf163
1991.08.10 <67574255+ImPaper@users.noreply.github.com>
2020-11-27 00:26:45 +0900

    3다수

---
+++ year2020/month11/day1124/samdasu1124.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1124
+{
+    class samdasu1124
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+            string B = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(A + B);
+
+            A = A.Substring(0, n);
+            B = B.Substring(n);
+            
+
+            Console.WriteLine(A + B);
+        }
+    }
+}
