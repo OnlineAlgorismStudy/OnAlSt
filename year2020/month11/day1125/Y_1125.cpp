f1b6d94df98077173abc9a28cc72dcebd5083bb1
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-11-27 16:25:36 +0900

    2020.11.27 : upload algo by Y

---
+++ year2020/month11/day1125/Y_1125.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	cout << stoi(s1) * stoi(s2) << endl;
+	return 0;
+}
