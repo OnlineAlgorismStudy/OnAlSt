41eb6151fe514f2971fd726454aa99634f8b41e4
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-12-12 11:18:42 +0900

    라봉
    라봉

---
+++ year2020/month11/day1126/Labong_11_26.java
@@ -0,0 +1,32 @@
+package november;
+
+import java.util.Scanner;
+
+public class Labong_11_26 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		double jNum = sc.nextDouble();
+		String str = sc.next();
+		
+		str = num+""+jNum+""+str.replaceAll(" ","");
+		num = str.length();
+		
+		for(int i=0; i<num; i++) {
+			if(i < num/2) {
+				System.out.print(str.charAt(i));
+			}else {
+				if(i == num/2) {
+					System.out.println();
+				}
+				System.out.print(str.charAt(i));
+			}
+			
+		}
+		sc.close();
+	}
+
+}
