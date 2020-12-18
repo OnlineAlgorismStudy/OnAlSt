b2b5b628dae548050653fe588225f613f34853b7
yeeun <33935840+pyeeun@users.noreply.github.com>
2020-12-18 22:12:52 +0900

    2020.12.15 : upload algo by Y

---
+++ year2020/month12/day1215/Y_1215.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n) {
+    int answer = 0;
+    while(n != 0) {
+        answer += n % 10;
+        n /= 10;
+    }
+    return answer;
+}
