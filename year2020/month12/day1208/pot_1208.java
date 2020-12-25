2287cdedbcb346bb2f9d0920709a777b2be24701
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-12-25 21:33:53 +0900

    Create pot_1208.java

---
+++ year2020/month12/day1208/pot_1208.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0 ; i < n; i++){
+            if(i % 2 == 2){
+                answer += "수";
+            }else{
+                answer += "박";
+            }    
+        }
+        
+        
+        return answer;
+    }
+}
