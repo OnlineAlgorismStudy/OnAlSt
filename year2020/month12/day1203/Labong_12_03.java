d99a741fff74971793fc43bee6559d4b2a180e08
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2021-01-05 22:42:10 +0900

    라봉
    라봉

--- year2020/month12/day1203/Labong_12_03.java
+++ year2020/month12/day1203/Labong_12_03.java
@@ -1,18 +1,17 @@
 class Solution {
     public long solution(int a, int b) {
-		long sum = 0;
-		
-		if(a<b) {
-			for(int i=a; i<=b; i++) {
-				sum += i;
-			}
-		}else if(a>b){
-			for(int i=b; i<=a; i++) {
-				sum += i;
-			}
-		}else{
-            sum +=a;
-        }
-		return sum;
+	long sum = 0;
+	if(a<b) {
+		for(int i=a; i<=b; i++) {
+			sum += i;
+		}
+	}else if(a>b){
+		for(int i=b; i<=a; i++) {
+			sum += i;
+		}
+	}else{
+		sum +=a;
+	 }
+	return sum;
     }
 }
