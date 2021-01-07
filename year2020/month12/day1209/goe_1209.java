af675dc4a84472f93dacd070560e175836202322
킹강 <52662748+itkk0306@users.noreply.github.com>
2021-01-07 14:02:46 +0900

    괴 늦은 로드_1209(1)

---
+++ year2020/month12/day1209/goe_1209.java
@@ -0,0 +1,15 @@
+package month12;
+
+public class goe_1209 {
+    public static int solution(String s) {
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 테스트용 메인 함수 1점
+        System.out.println(solution("-1234"));
+    }
+}
