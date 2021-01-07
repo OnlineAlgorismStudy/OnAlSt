e456dd5a58785b5411f106799ac399e8c49c8b1c
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2021-01-07 23:10:40 +0900

    라봉
    라봉

--- year2020/month12/day1207/Labong_12_07.java
+++ year2020/month12/day1207/Labong_12_07.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 class Solution {
     public boolean solution(String s) {
         
-    boolean check = check = Pattern.matches("^[0-9]*$", s);      
+    boolean check = Pattern.matches("^[0-9]*$", s);      
         
         return check;
     }
