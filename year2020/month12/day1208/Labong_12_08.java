0c27fb62e48421fdd2a00b38cff4bf7e77290d39
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2021-01-18 23:36:26 +0900

    라봉
    라봉

---
+++ year2020/month12/day1208/Labong_12_08.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for(int i=0; i<n; i++){
+            if(i % 2 == 0){
+                answer += "수";    
+            }else{
+                answer += "박";    
+            }
+        }
+        return answer;
+    }
+}
